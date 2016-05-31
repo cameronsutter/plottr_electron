a18d2a373f2e44b6e568d5e2e749178443d1fe7a
Cameron Sutter <cameronsutter0@gmail.com>
2016-05-31 16:15:41 -0600

    improved build
    the zipped OS X version of Plottr was always much bigger with the new
    build script than before. To fix this, I  made the script compress it
    to a .dmg instead of a .zip which is slightly more annoying but much
    smaller

--- bin/build.sh
+++ bin/build.sh
@@ -8,12 +8,13 @@ echo "organizing..."
 # move all the builds to one folder
 mkdir $npm_package_version
 mv Plottr-darwin-x64 $npm_package_version/OSX
-mv Plottr-win32-ia32 $npm_package_version/win32
-mv Plottr-win32-x64 $npm_package_version/win64
+# mv Plottr-win32-ia32 $npm_package_version/win32
+# mv Plottr-win32-x64 $npm_package_version/win64
 
 echo "zipping..."
 # zip all the builds
-zip -r -X Plottr-$npm_package_version.zip $npm_package_version/OSX/Plottr.app 1> /dev/null
+# tar -zcf Plottr-$npm_package_version.tar.gz $npm_package_version/OSX/Plottr.app 1> /dev/null
+hdiutil create -format UDZO -srcfolder $npm_package_version/OSX/Plottr.app Plottr-$npm_package_version.dmg 1> /dev/null
 zip -r -X Plottr-$npm_package_version-win64.zip $npm_package_version/win64 1> /dev/null
 zip -r -X Plottr-$npm_package_version-win32.zip $npm_package_version/win32 1> /dev/null
 

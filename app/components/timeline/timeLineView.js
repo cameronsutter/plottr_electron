69ea03122eccf5a5bd6ba359c00c4b6df096938a
Cameron Sutter <cameronsutter0@gmail.com>
2016-07-28 17:01:07 -0600

    sub navigation menu
    non-functional menu buttons

--- app/components/timeline/timeLineView.js
+++ app/components/timeline/timeLineView.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
+import { Navbar, Nav, NavItem, Button, ButtonGroup, Glyphicon } from 'react-bootstrap'
 import SceneListView from 'components/timeline/sceneListView'
 import LineListView from 'components/timeline/lineListView'
 
@@ -36,12 +37,33 @@ class TimeLineView extends Component {
 
   render () {
     return (
-      <div id='timelineview-root'>
+      <div id='timelineview-root' className='container-with-sub-nav'>
+        <Navbar className='subnav__container'>
+          <Nav bsStyle='pills' >
+            <NavItem>
+              <Button bsSize='small' ><Glyphicon glyph='filter' /> Filter</Button>
+            </NavItem>
+            <NavItem>
+              <span>Zoom: </span>
+              <ButtonGroup bsSize='small'>
+                <Button ><Glyphicon glyph='plus-sign' /></Button>
+                <Button >100%</Button>
+                <Button ><Glyphicon glyph='minus-sign' /></Button>
+              </ButtonGroup>
+            </NavItem>
+          </Nav>
+        </Navbar>
         <SceneListView filterItem={this.filterItem.bind(this)} filterList={this.filterList.bind(this)} filteredItems={this.state.filteredItems} />
         <LineListView sceneMap={this.sceneMapping()} filteredItems={this.state.filteredItems} />
       </div>
     )
   }
+  // <div className='subnav__container well'>
+  // <Button bsSize='small' ><Glyphicon glyph='filter' /> Filter</Button>
+  // <Button bsSize='small' ><Glyphicon glyph='zoom-in' /> Zoom In</Button>
+  // <Button bsSize='small' ><Glyphicon glyph='search' /> Normal</Button>
+  // <Button bsSize='small' ><Glyphicon glyph='zoom-out' /> Zoom Out</Button>
+  // </div>
 
   sceneMapping () {
     var mapping = {}

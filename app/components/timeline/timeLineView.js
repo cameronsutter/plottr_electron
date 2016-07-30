f8fc05ee50b33cb97f88c3cb83f3de7784a3661d
Cameron Sutter <cameronsutter0@gmail.com>
2016-07-29 19:00:20 -0600

    simple zoom in and out
    refs #29
    lets you zoom in and out, fit the whole plot in the view, and reset
    back to normal zoom

--- app/components/timeline/timeLineView.js
+++ app/components/timeline/timeLineView.js
@@ -4,10 +4,15 @@ import { Navbar, Nav, NavItem, Button, ButtonGroup, Glyphicon } from 'react-boot
 import SceneListView from 'components/timeline/sceneListView'
 import LineListView from 'components/timeline/lineListView'
 
+const ZOOM_STATES = [0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 3]
+const INITIAL_ZOOM_INDEX = 4
+const INITIAL_ZOOM_STATE = 'initial'
+const FIT_ZOOM_STATE = 'fit'
+
 class TimeLineView extends Component {
   constructor (props) {
     super(props)
-    this.state = {filteredItems: this.defaultFilteredItemsObj()}
+    this.state = {filteredItems: this.defaultFilteredItemsObj(), zoomState: INITIAL_ZOOM_STATE, zoomIndex: INITIAL_ZOOM_INDEX}
   }
 
   defaultFilteredItemsObj () {
@@ -35,9 +40,31 @@ class TimeLineView extends Component {
     this.setState({filteredItems: filteredItems})
   }
 
+  makeTransform () {
+    var elem = this.refs.timeline
+    var scale = ZOOM_STATES[this.state.zoomIndex]
+    if (this.state.zoomState === INITIAL_ZOOM_STATE) return {transform: INITIAL_ZOOM_STATE}
+    else if (this.state.zoomState === FIT_ZOOM_STATE) scale = window.outerWidth / elem.scrollWidth
+
+    return {transform: `scale(${scale}, ${scale})`, transformOrigin: 'left top'}
+  }
+
+  increaseZoomFactor () {
+    var newIndex = this.state.zoomIndex
+    if (newIndex < ZOOM_STATES.length - 1) newIndex++
+    this.setState({zoomState: null, zoomIndex: newIndex})
+  }
+
+  decreaseZoomFactor () {
+    var newIndex = this.state.zoomIndex
+    if (newIndex > 0) newIndex--
+    this.setState({zoomState: null, zoomIndex: newIndex})
+  }
+
   render () {
+    var styles = this.makeTransform()
     return (
-      <div id='timelineview-root' className='container-with-sub-nav'>
+      <div id='timelineview__container' className='container-with-sub-nav'>
         <Navbar className='subnav__container'>
           <Nav bsStyle='pills' >
             <NavItem>
@@ -46,15 +73,18 @@ class TimeLineView extends Component {
             <NavItem>
               <span>Zoom: </span>
               <ButtonGroup bsSize='small'>
-                <Button ><Glyphicon glyph='plus-sign' /></Button>
-                <Button >100%</Button>
-                <Button ><Glyphicon glyph='minus-sign' /></Button>
+                <Button onClick={this.increaseZoomFactor.bind(this)} ><Glyphicon glyph='plus-sign' /></Button>
+                <Button onClick={this.decreaseZoomFactor.bind(this)} ><Glyphicon glyph='minus-sign' /></Button>
+                <Button onClick={() => this.setState({zoomState: FIT_ZOOM_STATE, zoomIndex: INITIAL_ZOOM_INDEX})} >Fit</Button>
+                <Button onClick={() => this.setState({zoomState: INITIAL_ZOOM_STATE, zoomIndex: INITIAL_ZOOM_INDEX})} >Reset</Button>
               </ButtonGroup>
             </NavItem>
           </Nav>
         </Navbar>
-        <SceneListView filterItem={this.filterItem.bind(this)} filterList={this.filterList.bind(this)} filteredItems={this.state.filteredItems} />
-        <LineListView sceneMap={this.sceneMapping()} filteredItems={this.state.filteredItems} />
+        <div id='timelineview__root' ref='timeline' style={styles}>
+          <SceneListView filterItem={this.filterItem.bind(this)} filterList={this.filterList.bind(this)} filteredItems={this.state.filteredItems} />
+          <LineListView sceneMap={this.sceneMapping()} filteredItems={this.state.filteredItems} />
+        </div>
       </div>
     )
   }

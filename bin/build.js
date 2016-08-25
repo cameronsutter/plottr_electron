8f4fbc31b2cb0ad9f07b1133b959fa16c919f971
Cameron Sutter <cameronsutter0@gmail.com>
2016-08-24 19:00:12 -0600

    fix build … again
    also started a js build script that isn’t currently working, but it
    will be useful in the future

---
+++ bin/build.js
@@ -0,0 +1,37 @@
+var packager = require('electron-packager')
+var path = require('path')
+
+var options = {
+  name: 'Plottr',
+  dir: path.resolve('..'),
+  asar: true,
+  prune: true,
+  version: '1.3.2',
+  icons: 'icons/pblack',
+  out: path.resolve('/', 'Users', 'csutter', 'plottr_dist'),
+  protocol: ['plottr'],
+  'app-copyright': 'Copyright 2016 C. Louis S. (Cameron Sutter)',
+  'app-category-type': 'public.app-category.productivity',
+  'protocol-name': ['Plottr'],
+  arch: 'x64',
+  platform: 'darwin'
+}
+
+// macOS
+packager(options, function (errMac, appPaths) {
+  if (errMac) console.log('error building macOS:' + errMac)
+  else {
+    // win64
+    options.platform = 'win32'
+    packager(options, function (errWin64, appPaths) {
+      if (errWin64) console.log('error building win64:' + errWin64)
+      else {
+        // win32
+        options.arch = 'ia32'
+        packager(options, function (errWin32, appPaths) {
+          if (errWin32) console.log('error building win32:' + errWin32)
+        })
+      }
+    })
+  }
+})
--- bin/build.sh
+++ bin/build.sh
@@ -7,14 +7,14 @@ cd ~/plottr_dist
 echo "organizing..."
 # move all the builds to one folder
 mkdir $npm_package_version
-mv Plottr-darwin-x64 $npm_package_version/OSX
-mv Plottr-win32-ia32 $npm_package_version/win32
-mv Plottr-win32-x64 $npm_package_version/win64
+cp -R Plottr-darwin-x64 $npm_package_version/macOS && rm -R Plottr-darwin-x64
+cp -R Plottr-win32-ia32 $npm_package_version/win32 && rm -R Plottr-win32-ia32
+cp -R Plottr-win32-x64 $npm_package_version/win64 && rm -R Plottr-win32-x64
 
 echo "zipping..."
 # zip all the builds
-# tar -zcf Plottr-$npm_package_version.tar.gz $npm_package_version/OSX/Plottr.app 1> /dev/null
-hdiutil create -format UDZO -srcfolder $npm_package_version/OSX/Plottr.app Plottr-$npm_package_version.dmg 1> /dev/null
+# tar -zcf Plottr-$npm_package_version.tar.gz $npm_package_version/macOS/Plottr.app 1> /dev/null
+hdiutil create -format UDZO -srcfolder $npm_package_version/macOS/Plottr.app Plottr-$npm_package_version.dmg 1> /dev/null
 zip -r -X Plottr-$npm_package_version-win64.zip $npm_package_version/win64 1> /dev/null
 zip -r -X Plottr-$npm_package_version-win32.zip $npm_package_version/win32 1> /dev/null
 

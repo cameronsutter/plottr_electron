63ebdc687c7920c85a25621078079e01cab8e83c
Cameron Sutter <cameronsutter0@gmail.com>
2017-08-14 16:29:07 -0600

    log redux actions
    log redux actions in development mode. This is controlled by an
    environment variable (function in console)

---
+++ src/app/middlewares/logger.js
@@ -0,0 +1,14 @@
+const logger = store => next => action => {
+  if (process.env.NODE_ENV === 'dev' && process.env.LOGGER === 'true') {
+    console.info('----------')
+    console.info(`action: ${action.type}, payload: `, action)
+    console.info('before', store.getState())
+    const result = next(action)
+    console.info('after', store.getState())
+    return result
+  } else {
+    return next(action)
+  }
+}
+
+export default logger
--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -3,8 +3,9 @@ import rootReducer from '../reducers'
 import saver from '../middlewares/saver'
 import history from '../middlewares/history'
 import tracker from '../middlewares/tracker'
+import logger from '../middlewares/logger'
 
 export default function configureStore (initialState) {
-  const store = createStore(rootReducer, initialState, applyMiddleware(saver, history, tracker))
+  const store = createStore(rootReducer, initialState, applyMiddleware(saver, history, tracker, logger))
   return store
 }

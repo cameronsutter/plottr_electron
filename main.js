81a337c81d72a245bef1ba9048f9dd861834bd82
Cameron Sutter <cameronsutter0@gmail.com>
2017-12-15 15:55:45 -0700

    attempt 3

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ import * as newIds from './store/newIds'
 
 import * as ActionTypes from './constants/ActionTypes'
 
+import * as cardActions from './actions/cards'
+import * as characterActions from './actions/characters'
+import * as customAttributeActions from './actions/customAttributes'
+import * as lineActions from './actions/lines'
+import * as noteActions from './actions/notes'
+import * as placeActions from './actions/places'
+import * as sceneActions from './actions/scenes'
+import * as tagActions from './actions/tags'
+import * as uiActions from './actions/ui'
+import * as undoActions from './actions/undo'
+
 export rootReducer
 // export initialState
 // export lineColors

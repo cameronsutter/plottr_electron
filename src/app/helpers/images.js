71fc0f7ed24278c9181cf5c78a56e584cc29d76b
Cameron Sutter <cameronsutter0@gmail.com>
2020-03-13 07:32:51 -0600

    image compression & resizing

--- src/app/helpers/images.js
+++ src/app/helpers/images.js
@@ -1,32 +1,12 @@
-// NOTE: must use this in a file that has the DOM
+import Resizer from 'react-image-file-resizer'
 
-const reader = new FileReader()
-const width = 500
-
-// callback
-export function resizeToMaxWidth (dataURL, callback) {
-  const img = new Image()
-  img.src = dataURL
-  img.onload = () => {
-    const elem = document.createElement('canvas')
-    const scaleFactor = width / img.width
-    elem.width = width
-    elem.height = img.height * scaleFactor
-    const ctx = elem.getContext('2d')
-    ctx.drawImage(img, 0, 0, width, img.height * scaleFactor)
-    const data = ctx.canvas.toDataURL(img)
-
-    callback(data)
-  }
-}
+const maxWidth = 700
+const maxHeight = 500
+const format = 'JPEG' // PNG or WEBP
+const quality = 50 // out of 100
+const rotation = 0
+const output = 'base64'
 
 export function readImage (file, callback) {
-  reader.onload = (loadedFile => {
-    return e => {
-      resizeToMaxWidth(e.target.result, callback)
-    }
-  })(file)
-
-  // Read the image file as a data URL
-  reader.readAsDataURL(file)
+  Resizer.imageFileResizer(file, maxWidth, maxHeight, format, quality, rotation, callback, output)
 }

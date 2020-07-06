3e471d57e857d2cdf8129671d3d6e86888807cb3
Cameron Sutter <cameronsutter0@gmail.com>
2020-07-06 08:01:47 -0600

    analyzer improvments

--- src/app/components/analyzer/Analyzer.js
+++ src/app/components/analyzer/Analyzer.js
@@ -2,50 +2,44 @@ import React, { Component } from 'react'
 import PropTypes from 'react-proptypes'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Grid, Row, Col, Nav, NavItem } from 'react-bootstrap'
+import { get, set } from 'lodash' // possibly will be used to edit values
+import { Grid, Row, Col } from 'react-bootstrap'
 import ReactJson from 'react-json-view'
 import Inspector from 'react-json-inspector'
 import 'style-loader!css-loader!sass-loader!../../../../node_modules/react-json-inspector/json-inspector.css'
 
 class Analyzer extends Component {
-  state = {view: 'search'}
+  state = {path: null, tree: null}
 
   handleClick = ({key, value, path}) => {
-    if (!path.includes('.') && path != '') {
+    if (path != '') {
+      this.setState({path: path, tree: value})
     }
+  }
 
-    // maybe also if it's not a top level key, present that path in the right column
+  renderDetails () {
+    if (!this.state.tree) return null
+
+    return <div style={{padding: '10px'}}><ReactJson src={ this.state.tree } name={this.state.path} iconStyle='circle' /></div>
   }
 
   renderTree () {
     const { pltr } = this.props
-    if (this.state.view == 'search') {
-      return <div style={{padding: '10px'}}><Inspector data={ pltr } onClick={this.handleClick}/></div>
-    } else {
-      return <div style={{padding: '10px'}}><ReactJson src={ pltr } /></div>
-    }
+    return <div style={{padding: '10px'}}><Inspector data={ pltr } onClick={this.handleClick} searchOptions={{debounceTime: 300}}/></div>
   }
 
   render () {
     const { pltr } = this.props
-    return <div>
-      <h1>{pltr.file.version}{' '}<small>{pltr.file.fileName}</small></h1>
-
+    return <div style={{padding: '10px'}}>
+      <h3>{pltr.file.version}{' '}<small>{pltr.file.fileName}</small></h3>
       <Grid fluid>
         <Row>
-          <Col sm={12} md={6}>
-            <Nav bsStyle='tabs' activeKey={this.state.view} onSelect={key => this.setState({view: key})}>
-              <NavItem eventKey='search'>
-                Search
-              </NavItem>
-              <NavItem eventKey='edit'>
-                Edit
-              </NavItem>
-            </Nav>
+          <Col sm={12} md={5}>
             { this.renderTree() }
           </Col>
-          <Col sm={12} md={6}>
-
+          <Col sm={12} md={7}>
+            <h3>{this.state.path}</h3>
+            { this.renderDetails() }
           </Col>
         </Row>
       </Grid>

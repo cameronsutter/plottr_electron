3543ba86c7d311e63a26d8b285d47c885b11df27
Cameron Sutter <cameronsutter0@gmail.com>
2020-09-01 08:14:10 -0600

    improve place selectors

--- v2/selectors/places.js
+++ v2/selectors/places.js
@@ -1,9 +1,63 @@
 import { createSelector } from 'reselect'
 import { sortBy } from 'lodash'
+import { placeSortSelector, placeFilterSelector } from './ui'
 
 export const allPlacesSelector = state => state.places
+// this one also lives in ./customAttributes.js but it causes a circular dependency to import it here
+export const placeCustomAttributesSelector = state => state.customAttributes.places
 
 export const placesSortedAtoZSelector = createSelector(
   allPlacesSelector,
   (places) => sortBy(places, 'name')
+)
+
+export const placeFilterIsEmptySelector = createSelector(
+  placeFilterSelector,
+  placeCustomAttributesSelector,
+  (filter, attributes) => {
+    if (!filter) return true
+    const allAttributes = [{name:'tag'}, {name:'book'}, ...attributes]
+    return !allAttributes.some(attr => filter[attr.name] && filter[attr.name].length)
+  }
+)
+
+export const visibleSortedPlacesSelector = createSelector(
+  allPlacesSelector,
+  placeFilterSelector,
+  placeFilterIsEmptySelector,
+  placeSortSelector,
+  (allPlaces, filter, filterIsEmpty, sort) => {
+    if (!allPlaces.length) return []
+
+    let visible = allPlaces
+    if (!filterIsEmpty) {
+      visible = []
+      visible = allPlaces.filter(pl => {
+        return Object.keys(filter).some(attr => {
+          return filter[attr].some(val => {
+            if (attr == 'tag') {
+              return pl.tags.includes(val)
+            }
+            if (attr == 'book') {
+              return pl.bookIds.includes(val)
+            }
+            if (val == '') {
+              if (!pl[attr] || pl[attr] == '') return true
+            } else {
+              if (pl[attr] && pl[attr] == val) return true
+            }
+            return false
+          })
+        })
+      })
+    }
+
+    let sortOperands = sort.split('~')
+    let attrName = sortOperands[0]
+    let direction = sortOperands[1]
+    let sortOperand = attrName === 'name' ? [attrName, 'id'] : [attrName, 'name']
+    let sorted = sortBy(visible, sortOperand)
+    if (direction == 'desc') sorted.reverse()
+    return sorted
+  }
 )
--- v2/selectors/ui.js
+++ v2/selectors/ui.js
@@ -4,6 +4,8 @@ export const currentTimelineSelector = state => state.ui.currentTimeline
 export const timelineIsExpandedSelector = state => state.ui.timelineIsExpanded
 export const characterFilterSelector = state => state.ui.characterFilter
 export const characterSortSelector = state => state.ui.characterSort
+export const placeFilterSelector = state => state.ui.placeFilter
+export const placeSortSelector = state => state.ui.placeSort
 export const timelineFilterSelector = state => state.ui.timelineFilter
 
 export const isSeriesSelector = createSelector(

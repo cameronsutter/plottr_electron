9ce09c2472074d74c10d4044373ed2c32bd70bf3
Cameron Sutter <cameronsutter0@gmail.com>
2020-09-09 07:36:16 -0600

    WIP snowflake importer

--- src/common/importer/snowflake/importer.js
+++ src/common/importer/snowflake/importer.js
@@ -1,14 +1,16 @@
 import xml from 'xml-js'
 import fs from 'fs'
 import i18n from 'format-message'
-import { cloneDeep } from 'lodash'
+import { cloneDeep, uniq, keyBy, groupBy } from 'lodash'
 import { objectId, nextId } from '../../../app/store/newIds'
 import {
   book as defaultBook,
   note as defaultNote,
   character as defaultCharacter,
-  card as defaultCard
+  card as defaultCard,
+  chapter as defaultChapter
 } from '../../../../shared/initialState'
+import { nextPositionInBook } from '../../../app/helpers/lists'
 
 export default function Importer (path, isNewFile, state) {
   const importedXML = fs.readFileSync(path, 'utf-8')
@@ -27,33 +29,14 @@ export default function Importer (path, isNewFile, state) {
   storyLine(currentState, json, bookTitle, bookId)
   synopsis(currentState, json, bookTitle, bookId)
 
-  console.log(currentState.notes)
+  characters(currentState, json, bookId)
 
-  // characterInfoList
-  //   each one is a character
-  //
-  //   sentence -> description
-  //   paragraph -> notes
-  // add custom attributes for:
-  //   ambition
-  //   goal
-  //   conflict
-  //   epiphany
-  //   synopsis
-  //   [there can be a lot more, and we'll need a mapping from the name of the attr to what the UI calls it]
+  console.log('chapters before', currentState.chapters.length)
+  console.log('cards before', currentState.cards.length)
+  cards(currentState, json, bookId)
 
-  // sceneList
-  //   each one is a card in a chapter
-  //
-  //   chapter -> chapter (stack any that are in the same chapter)
-  //   sentence -> slate paragraph in description
-  //   paragraph -> slate paragraph in description
-  //   povName -> slate paragraph in description
-  //   date -> slate paragraph in description
-  //   location" value="" description="The location of the scene."/>
-  //   pageCountExpected -> slate paragraph in description
-  //   wordCountExpected -> slate paragraph in description
-  //   wordCountActual -> slate paragraph in description
+  console.log(currentState.chapters)
+  console.log(currentState.cards)
 
   return currentState
 }
@@ -71,6 +54,26 @@ function createNewNote (currentNotes, values) {
   const nextNoteId = nextId(currentNotes)
   const newNote = Object.assign({}, defaultNote, {id: nextNoteId, ...values})
   currentNotes.push(newNote)
+}
+
+function createNewCharacter (currentCharacters, values) {
+  const nextCharacterId = nextId(currentCharacters)
+  const newCharacter = Object.assign({}, defaultCharacter, {id: nextCharacterId, ...values})
+  currentCharacters.push(newCharacter)
+}
+
+function createNewCard (currentCards, values) {
+  const nextCardId = nextId(currentCards)
+  const newCard = Object.assign({}, defaultCard, {id: nextCardId, ...values})
+  currentCards.push(newCard)
+}
+
+function createNewChapter (currentChapters, values, bookId) {
+  const nextChapterId = nextId(currentChapters)
+  const position = nextPositionInBook(currentChapters, bookId)
+  const newChapter = Object.assign({}, defaultChapter, {id: nextChapterId, position: position, ...values})
+  currentChapters.push(newChapter)
+  return nextChapterId
 }
 
 function createSlateEditor (paragraphs) {
@@ -161,4 +164,134 @@ function synopsis (currentState, json, bookTitle, bookId) {
       })
     }
   }
+}
+
+function characters(currentState, json, bookId) {
+  const characterListNode = json['GContainer']['GContainer'].find(n => n['_attributes']['name'] == 'characterInfoList')
+  if (characterListNode && characterListNode['GContainer'] && characterListNode['GContainer'].length) {
+    let newCustomAttrs = []
+    characterListNode['GContainer'].forEach(chNode => {
+      if (chNode['GString'] && chNode['GString'].length) {
+        let notes = []
+        let values = chNode['GString'].reduce((acc, attr) => {
+          // get the right attr
+          const attrName = characterAttrMapping[attr['_attributes']['name']] || 'notes'
+          switch (attrName) {
+            case 'notes':
+              notes.push(attr['_attributes']['value'])
+              break
+            case 'name':
+              acc['name'] = attr['_attributes']['value']
+              break
+            case 'description':
+              acc['description'] = attr['_attributes']['value']
+              break
+            default:
+              acc[attrName] = attr['_attributes']['value'].replace(/&#10;/g, ' ') // &#10; is the newline character
+              // also create a custom attribute
+              newCustomAttrs.push(attrName)
+              break
+          }
+          return acc
+        }, {})
+        values['bookIds'] = [bookId]
+        const slateNotes = createSlateEditor(notes)
+        createNewCharacter(currentState.characters, {...values, notes: slateNotes})
+      }
+    })
+    createCustomCharacterAttributes(currentState, newCustomAttrs)
+  }
+}
+
+function createCustomCharacterAttributes(currentState, newCustomAttrs) {
+  // get names of current ones
+  const currentListByName = keyBy(currentState.customAttributes.characters, 'name')
+  // add new ones if they don't already exist
+  const customAttrs = uniq(newCustomAttrs).reduce((acc, attr) => {
+    if (!currentListByName[attr]) acc.push({name: attr, type: 'text'})
+    return acc
+  }, [])
+  currentState.customAttributes.characters = [
+    ...currentState.customAttributes.characters,
+    ...customAttrs,
+  ]
+}
+
+function cards(currentState, json, bookId) {
+  const sceneListNode = json['GContainer']['GContainer'].find(n => n['_attributes']['name'] == 'sceneList')
+  if (sceneListNode && sceneListNode['GContainer'] && sceneListNode['GContainer'].length) {
+    const scenesByChapter = groupBy(sceneListNode['GContainer'], (scene) => {
+      const node = scene['GInteger'].find(n => n['_attributes']['name'] == 'chapter')
+      if (node) return node['_attributes']['value']
+      return '1'
+    })
+    const chapterNumbers = Object.keys(scenesByChapter)
+    // what to do now? Need to create chapters and get their ids to use in the cards
+
+    sceneListNode['GContainer'].forEach(sNode => {
+      //   chapter -> chapter (stack any that are in the same chapter)
+
+      //   each one is a card in a chapter
+      //
+      //   sentence -> slate paragraph in description
+      //   paragraph -> slate paragraph in description
+      //   povName -> slate paragraph in description
+      //   date -> slate paragraph in description
+      //   location -> slate paragraph in description
+      //   pageCountExpected -> slate paragraph in description
+      //   wordCountExpected -> slate paragraph in description
+      //   wordCountActual -> slate paragraph in description
+    })
+  }
+}
+
+const characterAttrMapping = {
+  "name": "name",
+  "sentence": "description",
+  "paragraph": "notes",
+  "synopsis": "notes",
+  "ambition": "Ambition",
+  "goal": "Goal",
+  "conflict": "Conflict",
+  "epiphany": "Epiphany",
+  "othersSee": "How others see character",
+  "birthDate": "Date of Birth",
+  "age": "Age",
+  "height": "Height",
+  "weight": "Weight",
+  "ethnicity": "Ethnic Heritage",
+  "hairColor": "Hair Color",
+  "eyeColor": "Eye Color",
+  "physicalDescription": "Physical Description",
+  "dressStyle": "Style of Dressing",
+  "personality": "Personality Type",
+  "senseHumor": "Sense of Humor",
+  "religion": "Religion",
+  "politics": "Political Party",
+  "hobbies": "Hobbies",
+  "music": "Favorite Music",
+  "books": "Favorite Books",
+  "movies": "Favorite Movies",
+  "colors": "Favorite Colors",
+  "purseWallet": "Contents of purse or wallet",
+  "homeDescription": "Description of home",
+  "education": "Educational Background",
+  "work": "Work Experience",
+  "family": "Family",
+  "bestFriend": "Best Friend",
+  "maleFriends": "Male Friends",
+  "femaleFriends": "Female Friends",
+  "enemies": "Enemies",
+  "bestMemory": "Best childhood memory",
+  "worstMemory": "Worst childhood memory",
+  "characterization": "One-line characterization",
+  "strongTrait": "Strongest character trait",
+  "weakTrait": "Weakest character trait",
+  "paradox": "Character's Paradox",
+  "hope": "Greatest Hope",
+  "fear": "Deepest Fear",
+  "philosophy": "Philosophy of Life",
+  "seesSelf": "How character sees self",
+  "values": "Values",
+  "change": "How character will change",
 }

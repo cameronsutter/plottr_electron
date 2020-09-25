990ebc49f700e5fce0affd9474ac73d76c90b40b
Cameron Sutter <cameronsutter0@gmail.com>
2020-09-25 07:50:12 -0600

    add windows fonts

--- src/app/helpers/fonts.js
+++ src/app/helpers/fonts.js
@@ -1,7 +1,7 @@
 import { is } from 'electron-util'
 
 let darwinFonts = ['Andale Mono', 'Arial', 'Arial Black', 'Brush Script MT', 'Comic Sans MS', 'Courier New', 'Forum', 'Georgia', 'Impact', 'Times New Roman', 'Trebuchet MS', 'Verdana']
-let windowsFonts = []
+let windowsFonts = ['Arial', 'Arial Black', 'Calibri', 'Cambria', 'Comic Sans MS', 'Courier New', 'Forum', 'Impact', 'Segoe Script', 'Tahoma', 'Times New Roman', 'Trebuchet MS', 'Verdana']
 let plottrFonts = ['Forum', 'IBM Plex Serif', 'Lato', 'Yellowtail']
 let recentlyUsed = ['Forum']
 

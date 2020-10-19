561a46f3f60a1e0940e628876b1a8152eafd92de
Cameron Sutter <cameronsutter0@gmail.com>
2020-10-19 08:19:41 -0600

    only use mixpanel for paid users, not free trial users

--- src/app/middlewares/tracker.js
+++ src/app/middlewares/tracker.js
@@ -1,11 +1,13 @@
 import { ADD_LINES_FROM_TEMPLATE, ADD_CARD } from 'constants/ActionTypes'
 import { MPQ } from './helpers'
+import USER from '../../common/utils/user_info'
 
 const WHITE_LIST = [ADD_LINES_FROM_TEMPLATE]
 
 const tracker = store => next => action => {
   const result = next(action)
   if (!WHITE_LIST.includes(action.type)) return result
+  if (!USER.get('payment_id')) return result
 
   const state = store.getState()
   if (state && state.ui && state.file) {
--- src/common/utils/mixpanel.js
+++ src/common/utils/mixpanel.js
@@ -1,12 +1,10 @@
 import mixpanel from 'mixpanel-browser'
 import USER from './user_info'
-import { machineIdSync } from 'node-machine-id'
 
 export default function initMixpanel () {
-  mixpanel.init('507cb4c0ee35b3bde61db304462e9351')
+  // only use mixpanel for paid users, not free trial users
   if (USER.get('payment_id')) {
+    mixpanel.init('507cb4c0ee35b3bde61db304462e9351')
     mixpanel.identify(USER.get('payment_id').toString())
-  } else {
-    mixpanel.identify(machineIdSync(true))
   }
 }

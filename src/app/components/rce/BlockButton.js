cf302c8d86ffb57dd6013336e2fa02dddb2d2e4e
Matt Sessions <me@matthewsessions.com>
2020-11-05 22:08:27 -0500

    Fix bug with title, subtitle, and block quote
    In fixing lists, I broke these other types

--- src/app/components/rce/BlockButton.js
+++ src/app/components/rce/BlockButton.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { Editor, Transforms } from 'slate'
 import { useSlate } from 'slate-react'
 import { Button } from 'react-bootstrap'
-import { LIST_TYPES } from './helpers'
+import {
+  LIST_TYPES,
+  HEADING_TYPES,
+} from './helpers'
 
 const BlockButton = ({ format, icon }) => {
   const isBlockActive = (editor, format) => {
@@ -13,7 +16,7 @@ const BlockButton = ({ format, icon }) => {
     return !!match
   }
 
-  const toggleBlock = (editor, format) => {
+  const handleList = (editor, format) => {
     const isInList = Editor.isInList(editor, editor.selection)
 
     Transforms.unwrapNodes(editor, {
@@ -60,6 +63,70 @@ const BlockButton = ({ format, icon }) => {
           at: parentPath,
         });
       }
+    }
+  }
+
+  const handleHeadings = (editor, format) => {
+    const isActive = isBlockActive(editor, format);
+    if (isActive) {
+      Transforms.unwrapNodes(editor, {
+        match: n => n.type === format,
+        split: true
+      });
+      return;
+    }
+
+    const [isInHeading] = Editor.nodes(editor, {
+      match: n => HEADING_TYPES.includes(n.type),
+    });
+
+    // if the node is in a heading (won't be the same type
+    // since that is handled above) we have to remove the 
+    // other heading as they can't be nested 
+    if (Boolean(isInHeading)) {
+      Transforms.unwrapNodes(editor, {
+        match: n => HEADING_TYPES.includes(n.type),
+        split: true,
+      });
+    }
+
+    // wrap in the new heading type
+    Transforms.wrapNodes(editor, { type: format });
+  }
+
+  const handleBlockQuote = (editor, format) => {
+    const isActive = isBlockActive(editor, format)
+
+    if (isActive) {
+      Transforms.unwrapNodes(editor, {
+        match: n => n.type === format,
+        split: true,
+      });
+      return;
+    }
+    
+    // if the node is wrapped in a heading, we want the block quote around the heading
+    const [heading, headingPath] = Editor.parentOfType(editor, editor.selection, {
+      match: n => HEADING_TYPES.includes(n.type),
+    });
+    if (heading != null) {
+      Transforms.wrapNodes(editor, { type: format }, { at: headingPath });
+    } else {
+      Transforms.wrapNodes(editor, { type: format });
+    }
+  }
+
+  const toggleBlock = (editor, format) => {
+    if (LIST_TYPES.includes(format)) {
+      return handleList(editor, format)
+    }
+
+    if (HEADING_TYPES.includes(format)) {
+      return handleHeadings(editor, format)
+    }
+
+    if (format === 'block-quote') {
+      return handleBlockQuote(editor, format);
     }
   }
 

58d099bfb774650dbf2a2aefa1afa9fa41cd95f8
Matt Sessions <shichongrui@gmail.com>
2020-11-30 20:09:45 -0500

    Fix trial mode
    refs: BUG-87

--- bin/main_modules/constants.js
+++ bin/main_modules/constants.js
@@ -1,5 +1,11 @@
+let TRIAL_MODE = process.env.TRIALMODE === 'true';
+
+const getTrialMode = () => TRIAL_MODE;
+const setTrialMode = (value) => TRIAL_MODE = value;
+
 module.exports = {
-  TRIAL_MODE: process.env.TRIALMODE === 'true',
+  getTrialMode,
+  setTrialMode,
   NODE_ENV: process.env.NODE_ENV,
   ROLLBAR_ACCESS_TOKEN: process.env.ROLLBAR_ACCESS_TOKEN || ''
 }

bab9276e64da7239900cfca17cef5c0a59841bd3
Matt Sessions <me@matthewsessions.com>
2021-01-04 21:42:07 -0500

    Extend scroll area to include whole book container
    Fixes: BUG-105

--- src/dashboard/components/navigation/DashboardBody.js
+++ src/dashboard/components/navigation/DashboardBody.js
@@ -28,7 +28,7 @@ export default function DashboardBody ({currentView, setView}) {
 
   // no license and trial hasn't started (first time using the app)
   // OR no license and trial is expired
-  if (!licenseInfoSize && (!started || expired)) {
+  if (!licenseInfoSize && (!started || expired) && process.env.NODE_ENV !== 'development') {
     switch (currentView) {
       case 'help':
         return <Body><HelpHome/></Body>

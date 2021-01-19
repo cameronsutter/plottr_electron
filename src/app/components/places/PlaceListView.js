7e7e2e03201923145c9f0f4876c9ec74f9c83fe4
Edward John Steere <edward.steere@gmail.com>
2021-01-19 16:04:40 +0200

    Apply prettier fixes

--- src/app/components/places/PlaceListView.js
+++ src/app/components/places/PlaceListView.js
@@ -4,8 +4,19 @@ import PropTypes from 'react-proptypes'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import cx from 'classnames'
-import { Glyphicon, Nav, Navbar, NavItem, Button, Popover,
-  OverlayTrigger, Alert, Grid, Row, Col } from 'react-bootstrap'
+import {
+  Glyphicon,
+  Nav,
+  Navbar,
+  NavItem,
+  Button,
+  Popover,
+  OverlayTrigger,
+  Alert,
+  Grid,
+  Row,
+  Col,
+} from 'react-bootstrap'
 import CustomAttrFilterList from 'components/customAttrFilterList'
 import SortList from 'components/sortList'
 import * as PlaceActions from 'actions/places'
@@ -13,17 +24,20 @@ import * as CustomAttributeActions from 'actions/customAttributes'
 import * as UIActions from 'actions/ui'
 import PlaceView from 'components/places/placeView'
 import i18n from 'format-message'
-import { placeCustomAttributesThatCanChangeSelector, placeCustomAttributesRestrictedValues } from '../../selectors/customAttributes'
+import {
+  placeCustomAttributesThatCanChangeSelector,
+  placeCustomAttributesRestrictedValues,
+} from '../../selectors/customAttributes'
 import ErrorBoundary from '../../containers/ErrorBoundary'
 import PlaceItem from './PlaceItem'
 import { nextId } from '../../store/newIds'
 import { visibleSortedPlacesSelector, placeFilterIsEmptySelector } from '../../selectors/places'
-import CustomAttributeModal from '../dialogs/CustomAttributeModal';
+import CustomAttributeModal from '../dialogs/CustomAttributeModal'
 
-const modalStyles = {content: {width: '50%', marginLeft: '25%'}}
+const modalStyles = { content: { width: '50%', marginLeft: '25%' } }
 
 class PlaceListView extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       dialogOpen: false,
@@ -33,22 +47,22 @@ class PlaceListView extends Component {
     }
   }
 
-  static getDerivedStateFromProps (props, state) {
-    let returnVal = {...state}
+  static getDerivedStateFromProps(props, state) {
+    let returnVal = { ...state }
     const { visiblePlaces } = props
     returnVal.placeDetailId = PlaceListView.detailID(visiblePlaces, state.placeDetailId)
 
     return returnVal
   }
 
-  static detailID (places, placeDetailId) {
+  static detailID(places, placeDetailId) {
     if (places.length == 0) return null
 
     let id = places[0].id
 
     // check for the currently active one
     if (placeDetailId != null) {
-      let activePlace = places.find(pl => pl.id === placeDetailId)
+      let activePlace = places.find((pl) => pl.id === placeDetailId)
       if (activePlace) id = activePlace.id
     }
 
@@ -56,26 +70,26 @@ class PlaceListView extends Component {
   }
 
   editingSelected = () => {
-    this.setState({editingSelected: true})
+    this.setState({ editingSelected: true })
   }
 
   stopEditing = () => {
-    this.setState({editingSelected: false})
+    this.setState({ editingSelected: false })
   }
 
   closeDialog = () => {
-    this.setState({dialogOpen: false})
+    this.setState({ dialogOpen: false })
   }
 
   handleCreateNewPlace = () => {
     const id = nextId(this.props.places)
     this.props.actions.addPlace()
-    this.setState({placeDetailId: id, editingSelected: true})
+    this.setState({ placeDetailId: id, editingSelected: true })
   }
 
   handleType = () => {
     const attr = findDOMNode(this.refs.attrInput).value
-    this.setState({addAttrText: attr})
+    this.setState({ addAttrText: attr })
   }
 
   handleAddCustomAttr = (event) => {
@@ -88,51 +102,81 @@ class PlaceListView extends Component {
     const name = findDOMNode(this.refs.attrInput).value
     if (name == '' || this.props.restrictedValues.includes(name)) return // nothing? restricted value? no op
 
-    this.props.customAttributeActions.addPlaceAttr({name, type: 'text'})
-    this.setState({addAttrText: ''})
+    this.props.customAttributeActions.addPlaceAttr({ name, type: 'text' })
+    this.setState({ addAttrText: '' })
   }
 
   removeAttr = (attr) => {
     this.props.customAttributeActions.removePlaceAttr(attr.name)
-    this.setState({addAttrText: this.state.addAttrText}) // no op
+    this.setState({ addAttrText: this.state.addAttrText }) // no op
   }
 
   updateAttr = (index, oldAttribute, newAttribute) => {
     this.props.customAttributeActions.editPlaceAttr(index, oldAttribute, newAttribute)
   }
 
-  renderSubNav () {
+  renderSubNav() {
     const { ui, filterIsEmpty, uiActions } = this.props
-    let filterPopover = <Popover id='filter'>
-      <CustomAttrFilterList type='places' />
-    </Popover>
-    let filterDeclaration = <Alert onClick={() => uiActions.setPlaceFilter(null)}  bsStyle="warning"><Glyphicon glyph='remove-sign' />{"  "}{i18n('Place list is filtered')}</Alert>
+    let filterPopover = (
+      <Popover id="filter">
+        <CustomAttrFilterList type="places" />
+      </Popover>
+    )
+    let filterDeclaration = (
+      <Alert onClick={() => uiActions.setPlaceFilter(null)} bsStyle="warning">
+        <Glyphicon glyph="remove-sign" />
+        {'  '}
+        {i18n('Place list is filtered')}
+      </Alert>
+    )
     if (filterIsEmpty) {
       filterDeclaration = <span></span>
     }
-    let sortPopover = <Popover id='sort'>
-      <SortList type={'places'} />
-    </Popover>
+    let sortPopover = (
+      <Popover id="sort">
+        <SortList type={'places'} />
+      </Popover>
+    )
     let sortGlyph = 'sort-by-attributes'
     if (ui.placeSort.includes('~desc')) sortGlyph = 'sort-by-attributes-alt'
     return (
-      <Navbar className={cx('subnav__container', {darkmode: ui.darkMode})}>
-        <Nav bsStyle='pills' >
+      <Navbar className={cx('subnav__container', { darkmode: ui.darkMode })}>
+        <Nav bsStyle="pills">
           <NavItem>
-            <Button bsSize='small' onClick={this.handleCreateNewPlace}><Glyphicon glyph='plus' /> {i18n('New')}</Button>
+            <Button bsSize="small" onClick={this.handleCreateNewPlace}>
+              <Glyphicon glyph="plus" /> {i18n('New')}
+            </Button>
           </NavItem>
           <NavItem>
-            <Button bsSize='small' onClick={() => this.setState({dialogOpen: true})}><Glyphicon glyph='list' /> {i18n('Attributes')}</Button>
+            <Button bsSize="small" onClick={() => this.setState({ dialogOpen: true })}>
+              <Glyphicon glyph="list" /> {i18n('Attributes')}
+            </Button>
           </NavItem>
           <NavItem>
-            <OverlayTrigger containerPadding={20} trigger='click' rootClose placement='bottom' overlay={filterPopover}>
-              <Button bsSize='small'><Glyphicon glyph='filter' /> {i18n('Filter')}</Button>
+            <OverlayTrigger
+              containerPadding={20}
+              trigger="click"
+              rootClose
+              placement="bottom"
+              overlay={filterPopover}
+            >
+              <Button bsSize="small">
+                <Glyphicon glyph="filter" /> {i18n('Filter')}
+              </Button>
             </OverlayTrigger>
             {filterDeclaration}
           </NavItem>
           <NavItem>
-            <OverlayTrigger containerPadding={20} trigger='click' rootClose placement='bottom' overlay={sortPopover}>
-              <Button bsSize='small'><Glyphicon glyph={sortGlyph} /> {i18n('Sort')}</Button>
+            <OverlayTrigger
+              containerPadding={20}
+              trigger="click"
+              rootClose
+              placement="bottom"
+              overlay={sortPopover}
+            >
+              <Button bsSize="small">
+                <Glyphicon glyph={sortGlyph} /> {i18n('Sort')}
+              </Button>
             </OverlayTrigger>
           </NavItem>
         </Nav>
@@ -141,65 +185,71 @@ class PlaceListView extends Component {
   }
 
   renderVisiblePlaces = () => {
-    return this.props.visiblePlaces.map(pl => (
-      <PlaceItem key={pl.id} place={pl}
+    return this.props.visiblePlaces.map((pl) => (
+      <PlaceItem
+        key={pl.id}
+        place={pl}
         selected={pl.id == this.state.placeDetailId}
         startEdit={this.editingSelected}
         stopEdit={this.stopEditing}
-        select={() => this.setState({placeDetailId: pl.id})}
+        select={() => this.setState({ placeDetailId: pl.id })}
       />
     ))
   }
 
-  renderPlaces () {
-    return <div className={cx('place-list__list', 'list-group', {darkmode: this.props.ui.darkMode})}>
-      { this.renderVisiblePlaces() }
-    </div>
+  renderPlaces() {
+    return (
+      <div className={cx('place-list__list', 'list-group', { darkmode: this.props.ui.darkMode })}>
+        {this.renderVisiblePlaces()}
+      </div>
+    )
   }
 
-  renderPlaceDetails () {
-    let place = this.props.places.find(pl => pl.id === this.state.placeDetailId )
+  renderPlaceDetails() {
+    let place = this.props.places.find((pl) => pl.id === this.state.placeDetailId)
     if (place) {
-      return <ErrorBoundary>
-        <PlaceView key={`place-${place.id}`} place={place}
-          editing={this.state.editingSelected}
-          stopEditing={this.stopEditing}
-          startEditing={this.editingSelected}
-        />
-      </ErrorBoundary>
+      return (
+        <ErrorBoundary>
+          <PlaceView
+            key={`place-${place.id}`}
+            place={place}
+            editing={this.state.editingSelected}
+            stopEditing={this.stopEditing}
+            startEditing={this.editingSelected}
+          />
+        </ErrorBoundary>
+      )
     } else {
       return null
     }
   }
 
-  renderCustomAttributes () {
+  renderCustomAttributes() {
     if (!this.state.dialogOpen) {
-      return null;
+      return null
     }
-    return (
-      <CustomAttributeModal
-        type='places'
-        closeDialog={this.closeDialog}
-      />
-    )
+    return <CustomAttributeModal type="places" closeDialog={this.closeDialog} />
   }
 
-  render () {
+  render() {
     let klasses = 'secondary-text'
     if (this.props.ui.darkMode) klasses += ' darkmode'
     return (
-      <div className='place-list container-with-sub-nav'>
+      <div className="place-list container-with-sub-nav">
         {this.renderSubNav()}
         {this.renderCustomAttributes()}
         <Grid fluid>
           <Row>
             <Col sm={3}>
-              <h1 className={klasses}>{i18n('Places')}{' '}<Button onClick={this.handleCreateNewPlace}><Glyphicon glyph='plus' /></Button></h1>
+              <h1 className={klasses}>
+                {i18n('Places')}{' '}
+                <Button onClick={this.handleCreateNewPlace}>
+                  <Glyphicon glyph="plus" />
+                </Button>
+              </h1>
               {this.renderPlaces()}
             </Col>
-            <Col sm={9}>
-              {this.renderPlaceDetails()}
-            </Col>
+            <Col sm={9}>{this.renderPlaceDetails()}</Col>
           </Row>
         </Grid>
       </div>
@@ -219,7 +269,7 @@ PlaceListView.propTypes = {
   uiActions: PropTypes.object.isRequired,
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     places: state.present.places,
     visiblePlaces: visibleSortedPlacesSelector(state.present),
@@ -231,7 +281,7 @@ function mapStateToProps (state) {
   }
 }
 
-function mapDispatchToProps (dispatch) {
+function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(PlaceActions, dispatch),
     customAttributeActions: bindActionCreators(CustomAttributeActions, dispatch),
@@ -239,7 +289,4 @@ function mapDispatchToProps (dispatch) {
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PlaceListView)
+export default connect(mapStateToProps, mapDispatchToProps)(PlaceListView)

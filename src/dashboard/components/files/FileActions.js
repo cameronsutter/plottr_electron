8465d59ffc93413ccdccc2c90b4c9cbe112ddfab
Robins <Michelle.Robins@yrbrands.com>
2021-01-21 16:25:45 +0100

    Nifty issue kan-45 and kan-82; rename file from dot menu and rename preferences/options to settings

--- src/dashboard/components/files/FileActions.js
+++ src/dashboard/components/files/FileActions.js
@@ -4,9 +4,17 @@ import t from 'format-message'
 import { is } from 'electron-util'
 import { Dropdown, MenuItem, Glyphicon } from 'react-bootstrap'
 import { shell } from 'electron'
-import { deleteKnownFile, removeFromKnownFiles } from '../../../common/utils/known_files'
+import { deleteKnownFile, editKnownFilePath, displayFileName, removeFromKnownFiles } from '../../../common/utils/known_files'
 import DeleteConfirmModal from '../../../app/components/dialogs/DeleteConfirmModal'
 import { TEMP_FILES_PATH } from '../../../common/utils/config_paths'
+import { saveFile } from '../../../common/utils/files'
+import { ipcRenderer, remote } from 'electron'
+import i18n from 'format-message'
+import { store } from 'store/configureStore'
+import fs from 'fs'
+const { dialog } = remote
+const filters = [{name: 'Plottr file', extensions: ['pltr']}]
+const win = remote.getCurrentWindow()
 
 let showInMessage = t('Show in File Explorer')
 if (is.macos) {
@@ -29,7 +37,33 @@ export default function FileOptions ({missing, id, filePath, openFile}) {
     return <DeleteConfirmModal name={name} onDelete={deleteFile} onCancel={() => setDeleting(false)}/>
   }
 
+  const renameFile = () => {
+    console.log('start function')
+    // first, i need to open the dialog to ask the user where to save the file
+    const fileName = dialog.showSaveDialogSync(win, {filters, title: i18n('Where would you like to move it?')})
+    console.log('dialog')
+    // take the name they chose
+    if (fileName) {
+      let newFilePath = fileName.includes('.pltr') ? fileName : `${fileName}.pltr`
+      // rename it in known_files
+      editKnownFilePath (filePath, newFilePath)
+      // save file to disk:
+      // 1. read contents of the file
+      const contents = fs.readFileSync(filePath, 'utf-8')
+      // 2. save the contents to the new path
+      saveFile(newFilePath, contents)
+      // 3. delete the file at the old path
+      shell.moveItemToTrash(filePath, true)
+      // change the window's title
+      win.setRepresentedFilename(newFilePath)
+      win.setTitle(displayFileName(newFilePath))
+      // make sure it updates in the recent files list immediately
+      ipcRenderer.send('pls-tell-dashboard-to-reload-recents')     
+    }
+  }
+  
   const doTheThing = (eventKey) => {
+    console.log('do the thing')
     switch (eventKey) {
       case 'open':
         openFile(filePath, id)
@@ -37,7 +71,12 @@ export default function FileOptions ({missing, id, filePath, openFile}) {
       case 'show':
         shell.showItemInFolder(filePath)
         break
+      case 'rename':
+        console.log('rename case')
+        renameFile()
+        break
       case 'remove':
+        console.log('remove')
         removeFromKnownFiles(id)
         break
       case 'delete':
@@ -57,6 +96,7 @@ export default function FileOptions ({missing, id, filePath, openFile}) {
       <Dropdown.Menu>
         {missing ? null : <MenuItem eventKey='open'>{t('Open')}</MenuItem>}
         {missing ? null : <MenuItem eventKey='show'>{showInMessage}</MenuItem>}
+        {missing ? null : <MenuItem eventKey='rename'>{t ('Rename this file')}</MenuItem>}
         {isTemp ? null : <MenuItem eventKey='remove'>{t('Remove from this list')}</MenuItem>}
         {missing ? null : <MenuItem eventKey='delete'>{t('Delete')}</MenuItem>}
       </Dropdown.Menu>
--- src/dashboard/utils/tabs.js
+++ src/dashboard/utils/tabs.js
@@ -3,16 +3,12 @@ import t from 'format-message'
 import { IoMdContact, IoIosDocument, IoMdSettings, IoIosHelpBuoy, IoMdTime, IoIosBrowsers } from 'react-icons/io'
 import { is } from 'electron-util'
 
-let optionsTitle = t('Options')
-if (is.macos) {
-  optionsTitle = t('Preferences')
-}
 
 export const tabs = [
   {name: t('Account'), view: 'account', icon: <IoMdContact/>},
   {name: t('Files'), view: 'files', icon: <IoIosDocument/>},
   {name: t('Backups'), view: 'backups', icon: <IoMdTime/>},
-  {name: optionsTitle, view: 'options', icon: <IoMdSettings/>},
+  {name: t('Settings'), view: 'options', icon: <IoMdSettings/>},
   {name: t('Help'), view: 'help', icon: <IoIosHelpBuoy/>},
 ]
 // {name: t('Templates'), view: 'templates', icon: <IoIosBrowsers/>}, // alternative: IoIosAlbums

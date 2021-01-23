143fac738d92f2900836279f4db783be891bba7a
Edward John Steere <edward.steere@gmail.com>
2021-01-23 17:21:48 +0200

    Use chapters reducer from pltr

--- src/app/reducers/main.js
+++ src/app/reducers/main.js
@@ -5,13 +5,12 @@ import file from './file'
 import places from './places'
 import tags from './tags'
 import characters from './characters'
-import chapters from './chapters'
 import notes from './notes'
 import images from './images'
 import series from './series'
 import seriesLines from './seriesLines'
 
-const { customAttributes, lines, beats, books, cards, categories } = reducers
+const { customAttributes, lines, beats, books, cards, categories, chapters } = reducers
 
 const mainReducer = combineReducers({
   ui,

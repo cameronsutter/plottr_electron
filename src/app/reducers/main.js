21926f951bda9a807eda3c02d3ecaa94403dfa19
Edward John Steere <edward.steere@gmail.com>
2021-01-23 17:27:10 +0200

    Use characters reducer from pltr

--- src/app/reducers/main.js
+++ src/app/reducers/main.js
@@ -4,13 +4,12 @@ import ui from './ui'
 import file from './file'
 import places from './places'
 import tags from './tags'
-import characters from './characters'
 import notes from './notes'
 import images from './images'
 import series from './series'
 import seriesLines from './seriesLines'
 
-const { customAttributes, lines, beats, books, cards, categories, chapters } = reducers
+const { customAttributes, lines, beats, books, cards, categories, chapters, characters } = reducers
 
 const mainReducer = combineReducers({
   ui,

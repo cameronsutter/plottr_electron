005cdd5f8d2880fe5f399bdce2a547393b5f68e1
Edward John Steere <edward.steere@gmail.com>
2021-01-29 10:24:22 +0200

    Add tests for editing a card

--- src/app/reducers/__tests__/cards.test.js
+++ src/app/reducers/__tests__/cards.test.js
@@ -1,4 +1,9 @@
-import { ADD_CARD, ADD_CARD_IN_CHAPTER } from '../../constants/ActionTypes'
+import {
+  ADD_CARD,
+  ADD_CARD_IN_CHAPTER,
+  ADD_LINES_FROM_TEMPLATE,
+  EDIT_CARD_DETAILS,
+} from '../../constants/ActionTypes'
 import { card as defaultCard } from '../../../../shared/initialState'
 import cardsReducer from '../cards'
 import { isEqual, uniq } from 'lodash'
@@ -8,8 +13,19 @@ import { allCardsSelector } from '../../selectors/cards'
 // the state object.
 const mountToState = (cards) => ({ cards })
 const cardInState = ({ cards }, card) => cards.find((x) => isEqual(x, card))
-const emptyState = []
-const oneCardState = [defaultCard]
+const cardIdInState = ({ cards }, cardId) => cards.find(({ id }) => id === cardId)
+
+// Test fixtures
+const emptyState = cardsReducer(undefined, { type: 'blarg' })
+const oneCardState = cardsReducer(undefined, { type: ADD_CARD, card: defaultCard })
+const card1 = { ...defaultCard, id: 1 }
+const card2 = { ...defaultCard, id: 2 }
+const card3 = { ...defaultCard, id: 3 }
+const card4 = { ...defaultCard, id: 4 }
+const fourCardState = cardsReducer(undefined, {
+  type: ADD_LINES_FROM_TEMPLATE,
+  cards: [card1, card2, card3, card4],
+})
 
 describe('cardsReducer', () => {
   it('should produce a valid state object when supplied with an unknown event type', () => {
@@ -91,6 +107,87 @@ describe('cardsReducer', () => {
             [2, 1],
             [1, 0],
           ])
+        })
+      })
+    })
+  })
+  describe('add lines from template', () => {
+    it('should add all given cards to the state', () => {
+      expect(
+        allCardsSelector(
+          mountToState(
+            cardsReducer(emptyState, {
+              type: ADD_LINES_FROM_TEMPLATE,
+              cards: [card1, card2, card3, card4],
+            })
+          )
+        )
+      ).toEqual([card1, card2, card3, card4])
+    })
+  })
+  describe('edit card details', () => {
+    describe('given the default state', () => {
+      it('should produce the default state', () => {
+        expect(
+          cardsReducer(emptyState, {
+            type: EDIT_CARD_DETAILS,
+            id: 0,
+            attributes: {
+              best: true,
+            },
+          })
+        ).toEqual(emptyState)
+      })
+    })
+    describe('given a one card state', () => {
+      describe('and an id for a different card', () => {
+        it('should produce the same one card state', () => {
+          expect(
+            cardsReducer(oneCardState, {
+              type: EDIT_CARD_DETAILS,
+              id: 10,
+              attributes: {
+                best: true,
+              },
+            })
+          ).toEqual(oneCardState)
+        })
+      })
+      describe('and the id of that card', () => {
+        it('should edit that card', () => {
+          expect(
+            allCardsSelector(
+              mountToState(
+                cardsReducer(oneCardState, {
+                  type: EDIT_CARD_DETAILS,
+                  id: 1,
+                  attributes: {
+                    best: true,
+                  },
+                })
+              )
+            )[0]
+          ).toEqual({ ...defaultCard, best: true })
+        })
+      })
+    })
+    describe('given a four card state', () => {
+      describe('and the id of card2', () => {
+        it('should only edit card2', () => {
+          expect(
+            cardIdInState(
+              mountToState(
+                cardsReducer(fourCardState, {
+                  type: EDIT_CARD_DETAILS,
+                  id: 2,
+                  attributes: {
+                    best: true,
+                  },
+                })
+              ),
+              2
+            )
+          ).toEqual({ ...card2, best: true })
         })
       })
     })

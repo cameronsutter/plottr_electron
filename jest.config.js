a1f9e48c2138d93adbe2b20f7e57c272fa5709cf
Matt Sessions <me@matthewsessions.com>
2021-02-01 20:41:53 -0500

    Validating

--- jest.config.js
+++ jest.config.js
@@ -180,10 +180,10 @@ module.exports = {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(pltr)/)',
+    //   "\\.pnp\\.[^\\/]+$"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,

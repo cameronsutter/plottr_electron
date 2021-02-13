a8ea7c72d01a6efe785d036ee7786243603bcd27
Edward John Steere <edward.steere@gmail.com>
2021-02-13 15:52:35 +0200

    Ensure that all values are kept when merging arrays

--- src/dashboard/utils/window_manager.js
+++ src/dashboard/utils/window_manager.js
@@ -6,6 +6,7 @@ import { saveToTempFile } from '../../common/utils/temp_files'
 import { addToKnownFiles } from '../../common/utils/known_files'
 import Importer from '../../common/importer/snowflake/importer'
 import { emptyFile, migrateIfNeeded } from 'pltr/v2'
+import { mergeWith, isArray } from 'lodash'
 const win = remote.getCurrentWindow()
 const { dialog, app } = remote
 
@@ -31,6 +32,16 @@ export function openExistingFile() {
   }
 }
 
+const mergeRecursivelyWithArrayConcatStrategy = (object1, object2) => {
+  const concatenateArrays = (objValue, srcValue) => {
+    if (isArray(objValue)) {
+      return objValue.concat(srcValue)
+    }
+    return mergeRecursivelyWithArrayConcatStrategy(objValue, srcValue)
+  }
+  return mergeWith(object1, object2, concatenateArrays)
+}
+
 export function createNew(templateData) {
   const emptyPlottrFile = emptyFile(t('Untitled'), app.getVersion())
   const tempTemplateFilePath = saveToTempFile(templateData || { version: app.getVersion() })
@@ -44,7 +55,10 @@ export function createNew(templateData) {
         // Let the top level handler handle it
         throw error
       }
-      const mergedWithEmptyFile = Object.assign({}, emptyPlottrFile, templateData)
+      const mergedWithEmptyFile = mergeRecursivelyWithArrayConcatStrategy(
+        emptyPlottrFile,
+        templateData
+      )
       const filePath = saveToTempFile(mergedWithEmptyFile)
       const fileId = addToKnownFiles(filePath)
       openKnownFile(filePath, fileId)

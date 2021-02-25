df4517cf2f6d356a747c733dcd009d2ea70ab789
Edward John Steere <edward.steere@gmail.com>
2021-02-25 09:38:42 +0200

    Wire up and render hierarchy level editing to redux store

--- src/app/components/dialogs/BeatConfigModal.js
+++ src/app/components/dialogs/BeatConfigModal.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { PropTypes } from 'prop-types'
-import { Glyphicon } from 'react-bootstrap'
+import { Glyphicon, ButtonToolbar, Button } from 'react-bootstrap'
+import i18n from 'format-message'
 
 import PlottrModal from 'components/PlottrModal'
-import { selectors } from 'pltr/v2'
+import { selectors, actions, initialState } from 'pltr/v2'
 
 const { hierarchyLevelCount, sortedHierarchyLevels } = selectors
+const {
+  hierarchyLevels: { setLevelsOfHierarchy, setHierarchyLevels },
+} = actions
 
 const modalStyles = {
   overlay: {
@@ -26,12 +30,22 @@ const modalStyles = {
   },
 }
 
-const BeatConfigModal = ({ closeDialog, levelsOfHierarchy, hierarchyLevels }) => {
+const BeatConfigModal = ({
+  closeDialog,
+  levelsOfHierarchy,
+  hierarchyLevels,
+  setLevelsOfHierarchy,
+  setHierarchyLevels,
+}) => {
   const [stagedLevelsOfHierarchy, setStagedLevelsOfHierarchy] = useState(levelsOfHierarchy)
-
   useEffect(() => {
     setStagedLevelsOfHierarchy(levelsOfHierarchy)
   }, [levelsOfHierarchy])
+
+  const [stagedHierarchyLevels, setStagedHierarchyLevels] = useState(hierarchyLevels)
+  useEffect(() => {
+    setStagedHierarchyLevels(hierarchyLevels)
+  }, [hierarchyLevels])
 
   const HierarchyLevel = ({
     name,
@@ -42,7 +56,7 @@ const BeatConfigModal = ({ closeDialog, levelsOfHierarchy, hierarchyLevels }) =>
     borderStyle,
     backgroundColor,
   }) => (
-    <div className="beat-config-modal__levels-table-header">
+    <div className="beat-config-modal__levels-table-row">
       <div className="beat-config-modal__levels-table-cell">{name}</div>
       <div className="beat-config-modal__levels-table-cell">{autoNumber}</div>
       <div className="beat-config-modal__levels-table-cell">{textColor}</div>
@@ -63,16 +77,26 @@ const BeatConfigModal = ({ closeDialog, levelsOfHierarchy, hierarchyLevels }) =>
     backgroundColor: PropTypes.string.isRequired,
   }
 
+  const saveChangesAndClose = () => {
+    setHierarchyLevels(stagedHierarchyLevels)
+    closeDialog()
+  }
+
   return (
     <PlottrModal isOpen={true} onRequestClose={closeDialog} style={modalStyles}>
       <div className="beat-config-modal">
         <div className="beat-config-modal__hierarchy-count-wrapper">
-          <div className="beat-config-modal__hierarchy-count-label">Levels of hierarchy</div>
+          <div className="beat-config-modal__hierarchy-count-label">
+            {i18n('Levels of hierarchy')}
+          </div>
           <div className="beat-config-modal__hierarchy-count-controls">
             <button
               className="beat-config-modal__hierarchy-count-adjustment-control"
               onClick={() => {
                 setStagedLevelsOfHierarchy(stagedLevelsOfHierarchy - 1)
+                setStagedHierarchyLevels(
+                  stagedHierarchyLevels.slice(0, stagedHierarchyLevels.length - 1)
+                )
               }}
             >
               <Glyphicon glyph="minus" />
@@ -87,22 +111,31 @@ const BeatConfigModal = ({ closeDialog, levelsOfHierarchy, hierarchyLevels }) =>
               className="beat-config-modal__hierarchy-count-adjustment-control"
               onClick={() => {
                 setStagedLevelsOfHierarchy(stagedLevelsOfHierarchy + 1)
+                setStagedHierarchyLevels([...stagedHierarchyLevels, initialState.hierarchyLevel])
               }}
             >
               <Glyphicon glyph="plus" />
             </button>
           </div>
         </div>
-        <div className="beat-config-modal__levels-table-header">
-          <div className="beat-config-modal__levels-table-cell">Name</div>
-          <div className="beat-config-modal__levels-table-cell">Auto Number</div>
-          <div className="beat-config-modal__levels-table-cell">Text Color</div>
-          <div className="beat-config-modal__levels-table-cell">Text Size</div>
-          <div className="beat-config-modal__levels-table-cell">Border Color</div>
-          <div className="beat-config-modal__levels-table-cell">Border Style</div>
-          <div className="beat-config-modal__levels-table-cell">Background Color</div>
+        <div className="beat-config-modal__levels-table">
+          <div className="beat-config-modal__levels-table-header">
+            <div className="beat-config-modal__levels-table-cell">Name</div>
+            <div className="beat-config-modal__levels-table-cell">Auto Number</div>
+            <div className="beat-config-modal__levels-table-cell">Text Color</div>
+            <div className="beat-config-modal__levels-table-cell">Text Size</div>
+            <div className="beat-config-modal__levels-table-cell">Border Color</div>
+            <div className="beat-config-modal__levels-table-cell">Border Style</div>
+            <div className="beat-config-modal__levels-table-cell">Background Color</div>
+          </div>
+          {stagedHierarchyLevels.map(HierarchyLevel)}
         </div>
-        {hierarchyLevels.map(HierarchyLevel)}
+        <ButtonToolbar className="beat-config-modal__button-bar">
+          <Button onClick={closeDialog}>{i18n('Cancel')}</Button>
+          <Button bsStyle="success" onClick={saveChangesAndClose}>
+            {i18n('Save')}
+          </Button>
+        </ButtonToolbar>
       </div>
     </PlottrModal>
   )
@@ -112,6 +145,8 @@ BeatConfigModal.propTypes = {
   closeDialog: PropTypes.func.isRequired,
   levelsOfHierarchy: PropTypes.number.isRequired,
   hierarchyLevels: PropTypes.array.isRequired,
+  setLevelsOfHierarchy: PropTypes.func.isRequired,
+  setHierarchyLevels: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = (state) => ({
@@ -119,4 +154,6 @@ const mapStateToProps = (state) => ({
   hierarchyLevels: sortedHierarchyLevels(state.present),
 })
 
-export default connect(mapStateToProps)(BeatConfigModal)
+export default connect(mapStateToProps, { setLevelsOfHierarchy, setHierarchyLevels })(
+  BeatConfigModal
+)

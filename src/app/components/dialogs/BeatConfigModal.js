98671082a2cfecc941cd7c5a072f83ce62a023bc
Edward John Steere <edward.steere@gmail.com>
2021-02-25 09:42:37 +0200

    Refactor HierarchyLevel component into it's own file

--- src/app/components/dialogs/BeatConfigModal.js
+++ src/app/components/dialogs/BeatConfigModal.js
@@ -5,6 +5,8 @@ import { Glyphicon, ButtonToolbar, Button } from 'react-bootstrap'
 import i18n from 'format-message'
 
 import PlottrModal from 'components/PlottrModal'
+import HierarchyLevel from './HierarchyLevel'
+
 import { selectors, actions, initialState } from 'pltr/v2'
 
 const { hierarchyLevelCount, sortedHierarchyLevels } = selectors
@@ -46,36 +48,6 @@ const BeatConfigModal = ({
   useEffect(() => {
     setStagedHierarchyLevels(hierarchyLevels)
   }, [hierarchyLevels])
-
-  const HierarchyLevel = ({
-    name,
-    autoNumber,
-    textColor,
-    textSize,
-    borderColor,
-    borderStyle,
-    backgroundColor,
-  }) => (
-    <div className="beat-config-modal__levels-table-row">
-      <div className="beat-config-modal__levels-table-cell">{name}</div>
-      <div className="beat-config-modal__levels-table-cell">{autoNumber}</div>
-      <div className="beat-config-modal__levels-table-cell">{textColor}</div>
-      <div className="beat-config-modal__levels-table-cell">{textSize}</div>
-      <div className="beat-config-modal__levels-table-cell">{borderColor}</div>
-      <div className="beat-config-modal__levels-table-cell">{borderStyle}</div>
-      <div className="beat-config-modal__levels-table-cell">{backgroundColor}</div>
-    </div>
-  )
-
-  HierarchyLevel.propTypes = {
-    name: PropTypes.string.isRequired,
-    autoNumber: PropTypes.string.isRequired,
-    textColor: PropTypes.string.isRequired,
-    textSize: PropTypes.string.isRequired,
-    borderColor: PropTypes.string.isRequired,
-    borderStyle: PropTypes.string.isRequired,
-    backgroundColor: PropTypes.string.isRequired,
-  }
 
   const saveChangesAndClose = () => {
     setHierarchyLevels(stagedHierarchyLevels)

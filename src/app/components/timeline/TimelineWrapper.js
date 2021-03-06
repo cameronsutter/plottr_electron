551e6ade787939e729d6693b0e832ba1d210b57f
Edward John Steere <edward.steere@gmail.com>
2021-03-06 11:02:05 +0200

    Disable beatHierarchy modal if the corresponding feature is off

--- src/app/components/timeline/TimelineWrapper.js
+++ src/app/components/timeline/TimelineWrapper.js
@@ -288,11 +288,13 @@ class TimelineWrapper extends Component {
               <Glyphicon glyph="list" /> {i18n('Attributes')}
             </Button>
           </NavItem>
-          <NavItem>
-            <Button bsSize="small" onClick={this.openBeatConfig}>
-              <TiFlowChildren size={16} /> {i18n('Beats')}
-            </Button>
-          </NavItem>
+          {this.props.beatHierarchyIsOn ? (
+            <NavItem>
+              <Button bsSize="small" onClick={this.openBeatConfig}>
+                <TiFlowChildren size={16} /> {i18n('Beats')}
+              </Button>
+            </NavItem>
+          ) : null}
           <NavItem>
             <span className="subnav__container__label">{i18n('Zoom')}: </span>
             <ButtonGroup>
@@ -377,7 +379,7 @@ class TimelineWrapper extends Component {
   }
 
   renderBeatConfig() {
-    if (!this.state.beatConfigIsOpen) return null
+    if (!this.state.beatConfigIsOpen || !this.props.beatHierarchyIsOn) return null
 
     return <BeatConfigModal closeDialog={this.closeBeatConfig} />
   }
@@ -405,6 +407,7 @@ TimelineWrapper.propTypes = {
   isSmall: PropTypes.bool,
   isMedium: PropTypes.bool,
   isLarge: PropTypes.bool,
+  beatHierarchyIsOn: PropTypes.bool,
   filterIsEmpty: PropTypes.bool.isRequired,
   actions: PropTypes.object.isRequired,
 }
@@ -416,6 +419,7 @@ function mapStateToProps(state) {
     isSmall: selectors.isSmallSelector(state.present),
     isMedium: selectors.isMediumSelector(state.present),
     isLarge: selectors.isLargeSelector(state.present),
+    beatHierarchyIsOn: selectors.beatsHierarchyIsOn(state.present),
   }
 }
 

9f2d1f5317a5d147aec3d6109389918b2887754e
Cameron Sutter <cameronsutter0@gmail.com>
2021-03-15 16:31:51 -0600

    demo a select all/none

--- src/app/components/export/options/GeneralOptions.js
+++ src/app/components/export/options/GeneralOptions.js
@@ -2,11 +2,21 @@ import React from 'react'
 import i18n from 'format-message'
 import PropTypes from 'react-proptypes'
 import { CheckOption } from '../ExportOptions'
+import { Button } from 'react-bootstrap'
 
 export default function GeneralOptions({ type, options, updateOptions }) {
   return (
     <div className="list-wrapper">
       <div className="list-title">{i18n('General')}</div>
+      <div>
+        <Button bsSize="xs" bsStyle="link">
+          All
+        </Button>{' '}
+        /{' '}
+        <Button bsSize="xs" bsStyle="link">
+          None
+        </Button>
+      </div>
       <ul>
         {type == 'word' ? (
           <li>

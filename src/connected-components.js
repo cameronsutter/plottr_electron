9acf82dded7d22aa2c133ca67423a483e03bb987
Edward John Steere <edward.steere@gmail.com>
2021-04-07 16:23:11 +0200

    Fixup dependency on DeleteConfirmModal

--- src/connected-components.js
+++ src/connected-components.js
@@ -1,7 +1,5 @@
-import { connect, bindActionCreators } from 'react-redux'
 import { shell, ipcRenderer, remote } from 'electron'
 import { connections } from 'plottr_components'
-import * as pltr from 'pltr/v2'
 
 import {
   listTemplates,
@@ -13,29 +11,69 @@ import log from 'electron-log'
 import { createErrorReport } from './common/utils/full_error_report'
 import SETTINGS from './common/utils/settings'
 
-const connector = {
-  redux: {
-    connect,
-    bindActionCreators,
-  },
-  pltr,
-  platform: {
-    appVersion: '2021.4.6',
-    template: {
-      listTemplates,
-      listCustomTemplates,
-      deleteTemplate,
-      editTemplateDetails,
-      startSaveAsTemplate: () => {
-        const win = remote.getCurrentWindow()
-        ipcRenderer.sendTo(win.webContents.id, 'save-as-template-start', 'plotlines') // sends this message to this same process
-      },
+const platform = {
+  appVersion: '2021.4.6',
+  template: {
+    listTemplates,
+    listCustomTemplates,
+    deleteTemplate,
+    editTemplateDetails,
+    startSaveAsTemplate: () => {
+      const win = remote.getCurrentWindow()
+      ipcRenderer.sendTo(win.webContents.id, 'save-as-template-start', 'plotlines') // sends this message to this same process
     },
-    settings: SETTINGS,
-    openExternal: shell.openExternal,
-    createErrorReport,
-    log,
   },
+  settings: SETTINGS,
+  openExternal: shell.openExternal,
+  createErrorReport,
+  log,
 }
 
-export default connections.pltr(connector)
+const components = connections.pltr(platform)
+
+export const DeleteConfirmModal = components.DeleteConfirmModal
+export const ColorPickerColor = components.ColorPickerColor
+export const ItemsManagerModal = components.ItemsManagerModal
+export const ListItem = components.ListItem
+export const PlottrModal = components.PlottrModal
+export const EditAttribute = components.EditAttribute
+export const RichText = components.RichText
+export const editorRegistry = components.editorRegistry
+export const Image = components.Image
+export const ImagePicker = components.ImagePicker
+export const MiniColorPicker = components.MiniColorPicker
+export const Spinner = components.Spinner
+export const FunSpinner = components.FunSpinner
+export const InputModal = components.InputModal
+export const ColorPicker = components.ColorPicker
+export const Switch = components.Switch
+export const CardTemplateDetails = components.CardTemplateDetails
+export const PlotlineTemplateDetails = components.PlotlineTemplateDetails
+export const TemplateCreate = components.TemplateCreate
+export const TemplateEdit = components.TemplateEdit
+export const TemplatePicker = components.TemplatePicker
+export const Beamer = components.Beamer
+export const LanguagePicker = components.LanguagePicker
+export const CategoryPicker = components.CategoryPicker
+export const CharacterCategoriesModal = components.CharacterCategoriesModal
+export const CharacterDetails = components.CharacterDetails
+export const CharacterEditDetails = components.CharacterEditDetails
+export const CharacterItem = components.CharacterItem
+export const CharacterListView = components.CharacterListView
+export const CustomAttrFilterList = components.CustomAttrFilterList
+export const BookFilterList = components.BookFilterList
+export const CharacterCategoryFilterList = components.CharacterCategoryFilterList
+export const CharactersFilterList = components.CharactersFilterList
+export const PlacesFilterList = components.PlacesFilterList
+export const TagFilterList = components.TagFilterList
+export const GenericFilterList = components.GenericFilterList
+export const SortList = components.SortList
+export const CharacterView = components.CharacterView
+export const BookSelectList = components.BookSelectList
+export const ErrorBoundary = components.ErrorBoundary
+export const SelectList = components.SelectList
+export const TagLabel = components.TagLabel
+export const CustomAttributeModal = components.CustomAttributeModal
+export const SubNav = components.SubNav
+export const ProjectTemplateDetails = components.ProjectTemplateDetails
+export const CharacterTemplateDetails = components.CharacterTemplateDetails

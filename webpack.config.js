8dba444ca605ba47c2cd803f600e1528f989d1e1
Edward John Steere <edward.steere@gmail.com>
2021-04-12 12:28:02 +0200

    Fix the path to plottr_components in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -99,7 +99,7 @@ const rendererConfig = {
       dashboard: path.resolve(__dirname, 'src', 'dashboard'),
       test: path.resolve(__dirname, 'test'),
       'connected-components': path.resolve(__dirname, 'src', 'connected-components.js'),
-      plottr_components: path.resolve(__dirname, 'lib', 'plottr_components', 'dist'),
+      plottr_components: path.resolve(__dirname, 'lib', 'plottr_components', 'dist', 'components'),
     },
   },
   target: 'electron-renderer',

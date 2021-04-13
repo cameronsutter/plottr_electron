289dbdc796b9ee0ad3684abe0e670fa31f86bfe7
Edward John Steere <edward.steere@gmail.com>
2021-04-13 11:31:38 +0200

    Use filter lists from `plottr_components`
    They were all used in other components now used from
    `plottr_components`.

--- src/app/components/timeline/CardCell.js
+++ src/app/components/timeline/CardCell.js
@@ -70,9 +70,6 @@ class CardCell extends PureComponent {
     const numOfCards = cards.length
     const idxOfCards = numOfCards - 1
     return cards.map((card, idx) => {
-      if (card.color === null) {
-        card.color = 'null'
-      }
       const isLastOne = idx == cards.length - 1
       return (
         <div key={card.id}>

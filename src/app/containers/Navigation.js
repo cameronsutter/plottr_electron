9d4565f70de8afdcb3d971efd0b93070b52c735e
Edward John Steere <edward.steere@gmail.com>
2021-07-19 15:50:00 +0200

    Refactor `Navigation` to a function component

--- src/app/containers/Navigation.js
+++ src/app/containers/Navigation.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'react-proptypes'
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Navbar, Nav, NavItem, Button } from 'react-bootstrap'
 import { t as i18n } from 'plottr_locales'
@@ -16,12 +15,12 @@ import cx from 'classnames'
 const trialMode = SETTINGS.get('trialMode')
 const isDev = process.env.NODE_ENV == 'development'
 
-class Navigation extends Component {
-  handleSelect = (selectedKey) => {
-    this.props.actions.changeCurrentView(selectedKey)
+const Navigation = ({ isDarkMode, currentView, changeCurrentView }) => {
+  const handleSelect = (selectedKey) => {
+    changeCurrentView(selectedKey)
   }
 
-  renderTrialLinks() {
+  const renderTrialLinks = () => {
     if (!trialMode || isDev) return null
 
     return (
@@ -33,9 +32,9 @@ class Navigation extends Component {
     )
   }
 
-  Menu = () => {
+  const Menu = () => {
     return (
-      <div className={cx('project-nav__menu-popover', { darkmode: this.props.isDarkMode })}>
+      <div className={cx('project-nav__menu-popover', { darkmode: isDarkMode })}>
         <Button>Account</Button>
         <Button>Settings</Button>
         <Button>License</Button>
@@ -45,50 +44,46 @@ class Navigation extends Component {
     )
   }
 
-  render() {
-    const { currentView, isDarkMode } = this.props
-
-    return (
-      <Navbar className="project-nav" fluid inverse={isDarkMode}>
-        <Nav onSelect={this.handleSelect} activeKey={currentView} bsStyle="pills">
-          <BookChooser />
-          <NavItem eventKey="project">{i18n('Project')}</NavItem>
-          <NavItem eventKey="timeline">{i18n('Timeline')}</NavItem>
-          <NavItem eventKey="outline">{i18n('Outline')}</NavItem>
-          <NavItem eventKey="notes">{i18n('Notes')}</NavItem>
-          <NavItem eventKey="characters">{i18n('Characters')}</NavItem>
-          <NavItem eventKey="places">{i18n('Places')}</NavItem>
-          <NavItem eventKey="tags">{i18n('Tags')}</NavItem>
-          {isDev ? (
-            <NavItem eventKey="analyzer">
-              Analyzer<sup>(DEV)</sup>
-            </NavItem>
-          ) : null}
-        </Nav>
-        <Beamer inNavigation />
-        {this.renderTrialLinks()}
-        <Nav pullRight className="project-nav__options">
-          <OverlayTrigger
-            containerPadding={20}
-            trigger="click"
-            rootClose
-            placement="bottom"
-            overlay={this.Menu}
-          >
-            <Button>
-              <BsThreeDotsVertical />
-            </Button>
-          </OverlayTrigger>
-        </Nav>
-      </Navbar>
-    )
-  }
+  return (
+    <Navbar className="project-nav" fluid inverse={isDarkMode}>
+      <Nav onSelect={handleSelect} activeKey={currentView} bsStyle="pills">
+        <BookChooser />
+        <NavItem eventKey="project">{i18n('Project')}</NavItem>
+        <NavItem eventKey="timeline">{i18n('Timeline')}</NavItem>
+        <NavItem eventKey="outline">{i18n('Outline')}</NavItem>
+        <NavItem eventKey="notes">{i18n('Notes')}</NavItem>
+        <NavItem eventKey="characters">{i18n('Characters')}</NavItem>
+        <NavItem eventKey="places">{i18n('Places')}</NavItem>
+        <NavItem eventKey="tags">{i18n('Tags')}</NavItem>
+        {isDev ? (
+          <NavItem eventKey="analyzer">
+            Analyzer<sup>(DEV)</sup>
+          </NavItem>
+        ) : null}
+      </Nav>
+      <Beamer inNavigation />
+      {renderTrialLinks()}
+      <Nav pullRight className="project-nav__options">
+        <OverlayTrigger
+          containerPadding={20}
+          trigger="click"
+          rootClose
+          placement="bottom"
+          overlay={Menu}
+        >
+          <Button>
+            <BsThreeDotsVertical />
+          </Button>
+        </OverlayTrigger>
+      </Nav>
+    </Navbar>
+  )
 }
 
 Navigation.propTypes = {
   currentView: PropTypes.string.isRequired,
   isDarkMode: PropTypes.bool,
-  actions: PropTypes.object.isRequired,
+  changeCurrentView: PropTypes.object.isRequired,
 }
 
 function mapStateToProps(state) {
@@ -98,10 +93,6 @@ function mapStateToProps(state) {
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(actions.ui, dispatch),
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Navigation)
+export default connect(mapStateToProps, { changeCurrentView: actions.ui.changeCurrentView })(
+  Navigation
+)

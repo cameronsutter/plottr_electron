6928528b33e973b73c07d1725cb05ad0df042927
Edward Steere <edward.steere@gmail.com>
2021-10-12 12:28:29 +0200

    Add a note about where the RCE content that holds images is

---
+++ src/common/extract_images.js
@@ -0,0 +1,20 @@
+/** Places to look for image data:
+ *
+ * Card:
+ *  - any attr at root level
+ *  - (card).templates[x].attributes[y].value
+ *  - desrciption
+ *
+ * Characters:
+ *  - any attr at root level
+ *  - (character).templates[x].(attributeName).value
+ *  - notes
+ *
+ * Notes:
+ *  - any attr at root level
+ *  - content
+ *
+ * Places:
+ *  - any attr at root level
+ *  - notes
+ */

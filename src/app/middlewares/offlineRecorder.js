34ade328313bb563c44f069de7a28c3db52214fc
Edward Steere <edward.steere@gmail.com>
2021-11-17 11:58:37 +0200

    Only save an offline file if we're on a cloud file

--- src/app/middlewares/offlineRecorder.js
+++ src/app/middlewares/offlineRecorder.js
@@ -1,10 +1,14 @@
 import { ipcRenderer } from 'electron'
 
+import { selectors } from 'pltr/v2'
+
 const offlineRecorder = (store) => (next) => (action) => {
   const result = next(action)
 
   const state = store.getState().present
-  ipcRenderer.send('record-offline-backup', state)
+  if (selectors.isCloudFileSelector(state)) {
+    ipcRenderer.send('record-offline-backup', state)
+  }
 
   return result
 }

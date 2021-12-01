ccce9e208132a6b7e2708710ead6ab9922b95ef1
Edward John Steere <edward.steere@gmail.com>
2021-12-01 14:21:58 +0200

    Account for missing timestamps deciding what to do about resuming

--- src/resume.js
+++ src/resume.js
@@ -1,11 +1,13 @@
 export const resumeDirective = (offlineFile, cloudFile) => {
-  const originalTimeStamp = new Date(offlineFile.file.originalTimeStamp)
-  const currentTimeStamp = new Date(offlineFile.file.timeStamp)
-  const madeOfflineEdits = currentTimeStamp > originalTimeStamp
-  const madeEditsOnline = cloudFile.file.timeStamp.toDate() > originalTimeStamp
+  const originalTimeStamp =
+    offlineFile.file.originalTimeStamp && new Date(offlineFile.file.originalTimeStamp)
+  const currentTimeStamp = offlineFile.file.timeStamp && new Date(offlineFile.file.timeStamp)
+  const timestampsExist = currentTimeStamp && originalTimeStamp
+  const madeOfflineEdits = timestampsExist && currentTimeStamp > originalTimeStamp
+  const madeEditsOnline = timestampsExist && cloudFile.file.timeStamp.toDate() > originalTimeStamp
   const doNothing = !madeOfflineEdits
   const uploadOurs = madeOfflineEdits && !madeEditsOnline
-  const backupOurs = madeOfflineEdits && madeEditsOnline
+  const backupOurs = !timestampsExist || (madeOfflineEdits && madeEditsOnline)
 
   return [uploadOurs, backupOurs, doNothing]
 }

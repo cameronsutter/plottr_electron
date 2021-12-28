3d5050f4176c7c0b6158d92a2a49771b6b5f4ce2
Edward Steere <edward.steere@gmail.com>
2021-12-28 14:19:17 +0200

    Wire up `getTemplateById` to the redux store instead of side effect

--- src/common/utils/templates.js
+++ src/common/utils/templates.js
@@ -1,29 +1,13 @@
 import Store from 'electron-store'
-import { sortBy } from 'lodash'
 import {
-  getCustomTemplateById,
   deleteCustomTemplate,
   editCustomTemplate,
 } from '../../dashboard/utils/templates_from_firestore'
 
 const TEMPLATES_PATH = process.env.NODE_ENV == 'development' ? 'templates_dev' : 'templates'
-const templateStore = new Store({ name: TEMPLATES_PATH })
 const CUSTOM_TEMPLATES_PATH =
   process.env.NODE_ENV == 'development' ? 'custom_templates_dev' : 'custom_templates'
 export const customTemplateStore = new Store({ name: CUSTOM_TEMPLATES_PATH })
-
-export function getTemplateById(id) {
-  const templatesById = templateStore.get()
-  if (templatesById && templatesById[id]) return templatesById[id]
-
-  const customById = customTemplateStore.get()
-  if (customById && customById[id]) return customById[id]
-
-  const templateFromFirebase = getCustomTemplateById(id)
-  if (templateFromFirebase) return templateFromFirebase
-
-  return null
-}
 
 const isLocalTemplate = (id) => {
   const customById = customTemplateStore.get()
@@ -41,7 +25,6 @@ export function deleteTemplate(id, userId) {
 
 export function editTemplateDetails(id, { name, description, link }) {
   const info = {
-    ...getTemplateById(id),
     name: name,
     description: description,
     link: link,

443844352e4a9952d43c97c98d79be93090393b3
Edward John Steere <edward.steere@gmail.com>
2022-03-21 14:41:53 +0200

    Fix translations for `notifyUser`

--- src/notifyUser.js
+++ src/notifyUser.js
@@ -1,12 +1,11 @@
 import { t } from 'plottr_locales'
 import { shell } from 'electron'
 
-const messageForType = {
-  word: t('Your Plottr file was exported to a .docx file'),
-  scrivener: t('Your Plottr file was exported to a Scrivener project package'),
-}
-
 export function notifyUser(exportPath, type) {
+  const messageForType = {
+    word: t('Your Plottr file was exported to a .docx file'),
+    scrivener: t('Your Plottr file was exported to a Scrivener project package'),
+  }
   try {
     new Notification(t('File Exported'), { body: messageForType[type], silent: true })
   } catch (error) {

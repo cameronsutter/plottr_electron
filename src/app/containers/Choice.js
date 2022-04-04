20df14c210a35d47ea0f9470dc735b76e2fd7879
Edward Steere <edward.steere@gmail.com>
2022-04-04 15:04:43 +0200

    Fix pro onboarding flow

--- src/app/containers/Choice.js
+++ src/app/containers/Choice.js
@@ -3,19 +3,19 @@ import { PropTypes } from 'prop-types'
 import { connect } from 'react-redux'
 import cx from 'classnames'
 
-import { selectors } from 'pltr/v2'
+import { selectors, actions } from 'pltr/v2'
 import { ChoiceView } from 'connected-components'
 
 const reload = () => {
   window.location.reload()
 }
 
-const Choice = ({ darkMode }) => {
+const Choice = ({ darkMode, startProOnboardingFromRoot }) => {
   return (
     <div id="dashboard__react__root">
       <div className={cx('dashboard__main', { darkmode: darkMode })}>
         <div className="dashboard__account" style={{ width: '100vw' }}>
-          <ChoiceView goToAccount={reload} />
+          <ChoiceView goToAccount={reload} startProOnboarding={startProOnboardingFromRoot} />
         </div>
       </div>
     </div>
@@ -24,8 +24,9 @@ const Choice = ({ darkMode }) => {
 
 Choice.propTypes = {
   darkMode: PropTypes.bool,
+  startProOnboardingFromRoot: PropTypes.func.isRequired,
 }
 
-export default connect((state) => ({ darkMode: selectors.isDarkModeSelector(state.present) }))(
-  Choice
-)
+export default connect((state) => ({ darkMode: selectors.isDarkModeSelector(state.present) }), {
+  startProOnboardingFromRoot: actions.applicationState.startProOnboardingFromRoot,
+})(Choice)

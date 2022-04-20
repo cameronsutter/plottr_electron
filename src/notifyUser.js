80d061bccde962524df02ab1a5725d1ee1695d18
Edward Steere <edward.steere@gmail.com>
2022-04-20 13:28:38 +0200

    Show the notification created from the remote module

--- src/notifyUser.js
+++ src/notifyUser.js
@@ -8,7 +8,15 @@ export function notifyUser(exportPath, type) {
     scrivener: t('Your Plottr file was exported to a Scrivener project package'),
   }
   try {
-    new Notification(t('File Exported'), { body: messageForType[type], silent: true })
+    const notification = new Notification({
+      title: t('File Exported'),
+      body: messageForType[type],
+      silent: true,
+    })
+    notification.show()
+    setTimeout(() => {
+      notification.close()
+    }, 5000)
   } catch (error) {
     // ignore
     // on windows you need something called an Application User Model ID which may not work

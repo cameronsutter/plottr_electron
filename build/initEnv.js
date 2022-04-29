8792d5f48cfb76914c5287018e609c27862de378
Edward John Steere <edward.steere@gmail.com>
2022-04-29 08:37:33 +0200

    Fix dotenv-stringify usage

--- build/initEnv.js
+++ build/initEnv.js
@@ -1,9 +1,9 @@
-var fs = require('fs')
-var { stringify } = require('dotenv-stringify')
-var path = require('path')
+const fs = require('fs')
+const stringify = require('dotenv-stringify')
+const path = require('path')
 
 console.log('writing env variables')
-var env = {
+const env = {
   ROLLBAR_ACCESS_TOKEN: process.env.ROLLBAR_ACCESS_TOKEN,
   EDD_KEY: process.env.EDD_KEY,
   EDD_TOKEN: process.env.EDD_TOKEN,
@@ -13,6 +13,6 @@ var env = {
   NODE_ENV: 'production', // i'm pretty sure this is useless. I think it gets set by webpack
   TRIALMODE: false, // i'm pretty sure this is uselsss too
 }
-var envstr = stringify(env)
+const envstr = stringify(env)
 
 fs.writeFileSync('.env', envstr)

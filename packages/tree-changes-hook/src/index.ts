e9763894b8b83a85e8fbc902db224aa1c0ed2dd7
Edward John Steere <edward.steere@gmail.com>
2022-05-30 16:01:45 +0200

    Remove attributes that are part of circular structures in props

--- packages/tree-changes-hook/src/index.ts
+++ packages/tree-changes-hook/src/index.ts
@@ -3,20 +3,26 @@ import equal from '@gilbarbara/deep-equal';
 import treeChanges, { Data, KeyType, TreeChanges } from 'tree-changes';
 
 export default function useTreeChanges<T extends Data>(value: T) {
-  const previousValue = useRef(value);
-  const isEqual = equal(previousValue.current, value);
+  // Props that were found to be part of circular structures.
+  const strippedValue = {
+    ...value,
+    _owner: undefined,
+    alternative: undefined,
+  }
+  const previousValue = useRef(strippedValue);
+  const isEqual = equal(previousValue.current, strippedValue);
   const previousIsEqual = useRef(isEqual);
   const instance = useRef<TreeChanges<KeyType<T, typeof previousValue.current>>>(
-    treeChanges(previousValue.current, value),
+    treeChanges(previousValue.current, strippedValue),
   );
 
   useEffect(() => {
-    previousValue.current = value;
+    previousValue.current = strippedValue;
   });
 
   if (previousIsEqual.current !== isEqual || !isEqual) {
     previousIsEqual.current = isEqual;
-    instance.current = treeChanges(previousValue.current, value);
+    instance.current = treeChanges(previousValue.current, strippedValue);
   }
 
   return instance.current;

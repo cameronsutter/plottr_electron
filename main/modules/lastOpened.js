e8a508824659400db9b27847b3a149b3d2a74847
Edward John Steere <edward.steere@gmail.com>
2022-06-21 15:01:16 +0200

    Don't open offline files as last open files

--- main/modules/lastOpened.js
+++ main/modules/lastOpened.js
@@ -1,5 +1,6 @@
 import Store from 'electron-store'
 import { app } from 'electron'
+import { isOfflineFile } from './offlineFilePath'
 
 const LAST_OPENED_NAME = process.env.NODE_ENV == 'development' ? 'last_opened_dev' : 'last_opened'
 const lastOpenedFileStore = new Store({
@@ -9,10 +10,15 @@ const lastOpenedFileStore = new Store({
 })
 
 function lastOpenedFile() {
-  return lastOpenedFileStore.get('lastOpenedFilePath')
+  const lastFile = lastOpenedFileStore.get('lastOpenedFilePath')
+
+  if (isOfflineFile(lastFile)) return null
+  return lastFile
 }
 
 function setLastOpenedFilePath(filePath) {
+  if (isOfflineFile(filePath)) return
+
   lastOpenedFileStore.set('lastOpenedFilePath', filePath)
 }
 

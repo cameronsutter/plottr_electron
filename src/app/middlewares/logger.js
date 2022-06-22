6eca92e266e211740d881be89e76d3d25824674b
Edward John Steere <edward.steere@gmail.com>
2022-06-22 12:17:21 +0200

    Improve the action logger

--- src/app/middlewares/logger.js
+++ src/app/middlewares/logger.js
@@ -1,13 +1,19 @@
-import { shouldIgnoreAction } from './shouldIgnoreAction'
+import jsonDiff from 'json-diff'
 
 const logger = (store) => (next) => (action) => {
   if (LOGGER === 'true') {
-    if (shouldIgnoreAction(action)) return next(action)
-    console.info('----------')
+    console.info('----------START Redux Change Logger START----------')
     console.info(`action: ${action.type}, payload: `, action)
-    console.info('before', store.getState())
+    const oldState = store.getState().present
     const result = next(action)
-    console.info('after', store.getState())
+    const newState = store.getState().present
+    Object.keys(oldState).forEach((key) => {
+      if (oldState[key] !== newState[key]) {
+        console.log(`${key} changed:`)
+        console.log(jsonDiff.diffString(oldState[key], newState[key]))
+      }
+    })
+    console.info('----------END Redux Change Logger END----------')
     return result
   } else {
     return next(action)

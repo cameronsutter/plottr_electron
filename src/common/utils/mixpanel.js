daaf3c2a613c085a4d0062fb069edc53e1e49d20
Edward Steere <edward.steere@gmail.com>
2022-07-19 14:55:21 +0200

    Fix initialising mixpanel

--- src/common/utils/mixpanel.js
+++ src/common/utils/mixpanel.js
@@ -7,7 +7,7 @@ export default function initMixpanel() {
   const fileSystemAPIs = makeFileSystemAPIs(whenClientIsReady)
   if (mixpanel.__loaded) return
   // only use mixpanel for paid users, not free trial users
-  fileSystemAPIs.currentUserSettings((user) => {
+  fileSystemAPIs.currentUserSettings().then((user) => {
     if (user.payment_id && process.env.NODE_ENV != 'development') {
       mixpanel.init('507cb4c0ee35b3bde61db304462e9351')
       mixpanel.identify(user.payment_id.toString())

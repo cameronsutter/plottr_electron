fdd36c6941df316b19a892bc2a54ae41360a3244
Edward John Steere <edward.steere@gmail.com>
2022-08-17 07:53:58 +0200

    Keep more information about registered work

--- main/server/StatusManager.js
+++ main/server/StatusManager.js
@@ -19,11 +19,13 @@ class StatusManager {
     this.activeConnections.add(webSocket)
   }
 
-  registerTask(work) {
+  registerTask(work, name) {
     this.notifyBusy()
+    const thisWorkId = uuid()
+    this.tasks.push({ workId: thisWorkId, name })
     work.then(() => {
-      this.tasks = this.tasks.filter((otherTask) => {
-        return otherTask !== work
+      this.tasks = this.tasks.filter(({ workId }) => {
+        return workId !== thisWorkId
       })
       if (this.tasks.length === 0) {
         this.notifyDone()

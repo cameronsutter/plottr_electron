54a5675edfae76c4d530cfabcce804dc10c1e0c3
Edward John Steere <edward.steere@gmail.com>
2022-08-25 08:28:07 +0200

    Don't error out when the last opened file no longer exists

--- main/modules/lastOpened.js
+++ main/modules/lastOpened.js
@@ -4,12 +4,14 @@
  * assumption changes, please ensure that only one process is
  * responsible for writing to the last opened file store.
  */
-
+import fs from 'fs'
 import { app } from 'electron'
 import log from 'electron-log'
 
 import { isOfflineFile } from './offlineFilePath'
 import Store from '../lib/store'
+
+const { lstat } = fs.promises
 
 const LAST_OPENED_NAME = process.env.NODE_ENV == 'development' ? 'last_opened_dev' : 'last_opened'
 const lastOpenedFileStore = new Store(app.getPath('userData'), log, {
@@ -20,8 +22,21 @@ const lastOpenedFileStore = new Store(app.getPath('userData'), log, {
 function lastOpenedFile() {
   const lastFile = lastOpenedFileStore.get('lastOpenedFilePath')
 
-  if (isOfflineFile(lastFile)) return null
-  return lastFile
+  if (isOfflineFile(lastFile)) {
+    return Promise.resolve(null)
+  }
+
+  return lstat(lastFile)
+    .then(() => {
+      return lastFile
+    })
+    .catch((error) => {
+      if (error.code === 'ENOENT') {
+        return null
+      }
+      log.error('Error finding last opened file.  Refusing to open it.', error)
+      return null
+    })
 }
 
 function setLastOpenedFilePath(filePath) {

07167e30b57024dc973400cd25f1090b2a5caaa0
Edward John Steere <edward.steere@gmail.com>
2022-10-26 11:04:02 +0200

    Add tests for saving different values in the correct order

--- src/app/__tests__/saver.test.js
+++ src/app/__tests__/saver.test.js
@@ -52,4 +52,116 @@ describe('Saver', () => {
       })
     })
   })
+  describe('given a getState function that produces a sequence of values', () => {
+    describe('and a 100ms interval', () => {
+      it('should attempt to save ten different items in the right order', async () => {
+        let counter = 0
+        const getState = () => {
+          counter++
+          return {
+            counter,
+          }
+        }
+        const saveCalls = []
+        const saveFile = (...args) => {
+          saveCalls.push(args)
+          return Promise.resolve()
+        }
+        const backupFile = () => {
+          return Promise.resolve()
+        }
+        new Saver(getState, saveFile, backupFile, 100)
+        await new Promise((resolve) => {
+          setTimeout(resolve, 1050)
+        })
+        expect(saveCalls).toEqual([
+          [
+            {
+              counter: 1,
+            },
+          ],
+          [
+            {
+              counter: 2,
+            },
+          ],
+          [
+            {
+              counter: 3,
+            },
+          ],
+          [
+            {
+              counter: 4,
+            },
+          ],
+          [
+            {
+              counter: 5,
+            },
+          ],
+          [
+            {
+              counter: 6,
+            },
+          ],
+          [
+            {
+              counter: 7,
+            },
+          ],
+          [
+            {
+              counter: 8,
+            },
+          ],
+          [
+            {
+              counter: 9,
+            },
+          ],
+          [
+            {
+              counter: 10,
+            },
+          ],
+        ])
+      })
+    })
+    describe('and a 500ms interval', () => {
+      it('should attempt to save values in the correct order 2 times in one second', async () => {
+        let counter = 0
+        const getState = () => {
+          counter++
+          return {
+            counter,
+          }
+        }
+        const saveCalls = []
+        const saveFile = (...args) => {
+          saveCalls.push(args)
+          return Promise.resolve()
+        }
+        const backupFile = () => {
+          return Promise.resolve()
+        }
+        new Saver(getState, saveFile, backupFile, 500)
+        await new Promise((resolve) => {
+          setTimeout(resolve, 1050)
+        })
+        expect(saveCalls).toEqual([
+          [
+            {
+              counter: 1,
+            },
+          ],
+          [
+            {
+              counter: 2,
+            },
+          ],
+        ])
+      })
+    })
+  })
 })

1daf5f20e21d7d88c781b98aca4513e35c8ddd49
Edward John Steere <edward.steere@gmail.com>
2022-10-31 15:21:58 +0200

    Don't save partial files

--- src/app/save.js
+++ src/app/save.js
@@ -1,7 +1,19 @@
-import { selectors } from 'pltr/v2'
+import { difference } from 'lodash'
+
+import { selectors, SYSTEM_REDUCER_KEYS, emptyFile } from 'pltr/v2'
+
+const emptyFileState = emptyFile('DummyFile', '2022.11.2')
 
 export const saveFile = (whenClientIsReady, logger) => (state) => {
   return whenClientIsReady(({ saveFile, saveOfflineFile }) => {
+    const hasAllKeys = selectors.hasAllKeysSelector(state)
+    if (!hasAllKeys) {
+      const withoutSystemKeys = difference(Object.keys(state), SYSTEM_REDUCER_KEYS)
+      const missing = difference(Object.keys(emptyFileState), withoutSystemKeys)
+      const message = `File is missing keys (${missing}).  Refusing to save.`
+      logger.error('Missing keys', new Error(message))
+      return Promise.reject(message)
+    }
     const canSave = selectors.canSaveSelector(state)
     if (!canSave) {
       logger.warn('File is in a state that prohibits saving.  Refusing to auto-save.')

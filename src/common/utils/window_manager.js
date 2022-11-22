2353d524565c1737b29e0718b9118b7bd8be0051
Edward John Steere <edward.steere@gmail.com>
2022-11-22 09:30:02 +0200

    Remove node dependencies from `window_manager`

--- src/common/utils/window_manager.js
+++ src/common/utils/window_manager.js
@@ -1,6 +1,4 @@
 import { ipcRenderer } from 'electron'
-import { readFileSync } from 'fs'
-import path from 'path'
 
 import { t } from 'plottr_locales'
 import { helpers } from 'pltr/v2'
@@ -9,6 +7,7 @@ import logger from '../../../shared/logger'
 import { makeMainProcessClient } from '../../app/mainProcessClient'
 import { closeDashboard } from '../../dashboard-events'
 import { uploadProject } from './upload_project'
+import { whenClientIsReady } from '../../../shared/socket-client'
 
 const { showOpenDialog } = makeMainProcessClient()
 
@@ -29,36 +28,43 @@ export function openExistingFile(loggedIn, userId, email) {
     }
 
     if (loggedIn) {
-      try {
-        const fileText = readFileSync(filePath)
-        const file = JSON.parse(fileText)
-        return uploadProject(
-          {
-            ...file,
-            file: {
-              ...file.file,
-              fileName: path.basename(filePath, path.extname(filePath)),
-            },
-          },
-          email,
-          userId
-        ).then((response) => {
-          const fileId = response.data.fileId
-          if (!fileId) {
-            const message = `Tried to upload project from ${filePath} but the server replied without a fileId`
-            logger.error(message)
-            return Promise.reject(new Error(message))
-          }
-          logger.info('Successfully uploaded file')
-          const fileURL = helpers.file.fileIdToPlottrCloudFileURL(fileId)
-          openFile(fileURL, false)
-          closeDashboard()
-          return 'File opened...'
+      return whenClientIsReady(({ basename, extname, readFile }) => {
+        return extname(filePath).then((ext) => {
+          return basename(filePath, ext).then((fileName) => {
+            try {
+              return readFile(filePath).then((fileText) => {
+                const file = JSON.parse(fileText)
+                return uploadProject(
+                  {
+                    ...file,
+                    file: {
+                      ...file.file,
+                      fileName,
+                    },
+                  },
+                  email,
+                  userId
+                ).then((response) => {
+                  const fileId = response.data.fileId
+                  if (!fileId) {
+                    const message = `Tried to upload project from ${filePath} but the server replied without a fileId`
+                    logger.error(message)
+                    return Promise.reject(new Error(message))
+                  }
+                  logger.info('Successfully uploaded file')
+                  const fileURL = helpers.file.fileIdToPlottrCloudFileURL(fileId)
+                  openFile(fileURL, false)
+                  closeDashboard()
+                  return 'File opened...'
+                })
+              })
+            } catch (error) {
+              logger.error('Error uploading file', error)
+              return Promise.reject(error)
+            }
+          })
         })
-      } catch (error) {
-        logger.error('Error uploading file', error)
-        return Promise.reject(error)
-      }
+      })
     }
 
     ipcRenderer.send('add-to-known-files-and-open', helpers.file.filePathToFileURL(filePath))

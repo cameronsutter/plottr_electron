66a94ccbcd58a4139a4a2397e1e5889408f08c83
Edward John Steere <edward.steere@gmail.com>
2022-11-29 13:17:32 +0200

    Remove an unused import

--- src/app/components/Renamer.js
+++ src/app/components/Renamer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { PropTypes } from 'prop-types'
 import { connect } from 'react-redux'
 
